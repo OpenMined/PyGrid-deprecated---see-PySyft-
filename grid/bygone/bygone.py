b2b4e730766e8d9eff7e664388bc49f89e86edc6
Justin Patriquin <justin@sidestory.email>
2018-02-01 10:21:55 -0400

    move all ethereum stuff to bygone, correctly encode signed transaction

--- grid/bygone/bygone.py
+++ grid/bygone/bygone.py
@@ -1,14 +1,41 @@
+from ethereum import utils
+from ethereum import transactions
+import os
+import rlp
 import requests
+import binascii
 
-host = "127.0.0.1:3000"
+host = "http://127.0.0.1:3000"
 
 
-def add_experiment(experimentAddress, jobAddresses):
+def add_experiment(experimentAddress, jobAddresses, priv_key=None,
+                   account_address=None, returnAbi=None):
     payload = {'experimentAddress': experimentAddress,
-               'jobAddresses': jobAddresses}
+               'jobAddresses': jobAddresses, 'returnAbi': returnAbi,
+               'accountAddress': account_address}
 
     r = requests.post('{}/experiment'.format(host), json=payload)
     print("/experiment", r)
+
+    if returnAbi:
+        json = r.json()
+        abi = utils.decode_hex(json['abi'][2:])
+        nonce = json['nonce']
+        gas = json['estimatedGas']
+        contractAddress = json['contractAddress']
+
+        transaction = sign_transaction(nonce, abi, priv_key, gas,
+                                       contractAddress)
+
+        return send_raw_transaction('0x' + transaction)
+
+    return r.status_code
+
+
+def send_raw_transaction(transaction):
+    payload = {'rawTransaction': transaction}
+    r = requests.post(host + "/raw", json=payload)
+    print("/raw", r)
 
     return r.status_code
 
@@ -58,3 +85,28 @@ def get_result(jobAddress):
         return None
 
     return addr
+
+
+def create_wallet():
+    # TODO is urandom secure??? maybe need to use something else
+    private_key = utils.sha3(os.urandom(4096))
+
+    # TODO encrypt and store these keys somewhere for user?!?!
+    raw_address = utils.privtoaddr(private_key)
+    account_address = utils.checksum_encode(raw_address)
+
+    print("DO NOT LOSS PRIVATE KEY OR PUBLIC KEY NO WAY TO RECOVER")
+    print("PRIVATE KEY", private_key.hex(), "PUBLIC KEY", account_address)
+
+    return private_key, account_address
+
+
+def sign_transaction(nonce, abi, priv_key, gas, to):
+    gasprice = 18000000000
+
+    # [nonce, gasprice, startgas, to, value, data, v, r, s]
+    tx = transactions.Transaction(nonce, gasprice, gas, to, 5, abi)
+    signed_tx = tx.sign(priv_key)
+
+    ret = rlp.encode(signed_tx).hex()
+    return ret
