e0b170cbd412648089cd837e76bb45a9e127fa6a
Justin Patriquin <justin@sidestory.email>
2018-02-06 10:42:20 -0400

    remove echo

--- osx_installation.sh
+++ osx_installation.sh
@@ -13,10 +13,10 @@ fi
 #
 # Check if Git, go, Python3, and python dependencies are installed
 #
-which -s go || echo "installing go..."; brew install go
-which -s wget || echo "installing wget..."; brew install wget
-which -s git || echo "installing git..."; brew install git
-which -s python3 || echo "installing python3..."; brew install python3
+which -s go || brew install go
+which -s wget || brew install wget
+which -s git || brew install git
+which -s python3 || brew install python3
 sudo pip3 install -r requirements.txt
 
 wget https://dist.ipfs.io/ipfs-update/v1.5.2/ipfs-update_v1.5.2_darwin-amd64.tar.gz --no-check-certificate
