ca32687cdc4274cafd20d7373e018be71d3dfaf4
Justin Patriquin <justin@sidestory.email>
2018-02-06 10:40:21 -0400

    fixes some issues I had while running osx installation script

--- osx_installation.sh
+++ osx_installation.sh
@@ -13,21 +13,21 @@ fi
 #
 # Check if Git, go, Python3, and python dependencies are installed
 #
-which -s go || brew install go
-which -s wget || brew install wget
-which -s git || brew install git
-which -s python3 || brew install python3
+which -s go || echo "installing go..."; brew install go
+which -s wget || echo "installing wget..."; brew install wget
+which -s git || echo "installing git..."; brew install git
+which -s python3 || echo "installing python3..."; brew install python3
 sudo pip3 install -r requirements.txt
 
 wget https://dist.ipfs.io/ipfs-update/v1.5.2/ipfs-update_v1.5.2_darwin-amd64.tar.gz --no-check-certificate
 tar -xvf ipfs-update_v1.5.2_darwin-amd64.tar.gz
 cd ipfs-update
-sh install.sh 
+sh install.sh
 cd ../
 rm -rf ipfs-update*
 
 ipfs-update install latest
 ipfs init
-ipfs daemon --enable-pubsub-experiment  > ipfs.log &
+ipfs daemon --enable-pubsub-experiment  > ipfs.log 2> ipfs.log.err &
 
 python3 setup.py install
