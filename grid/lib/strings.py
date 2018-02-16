55d14481acff39eb16e9d12a628d978c23be96c3
Ben DeCoste <bendecoste@gmail.com>
2018-02-16 14:37:44 -0400

    compute art

--- grid/lib/strings.py
+++ grid/lib/strings.py
@@ -7,9 +7,31 @@ tree = f"""{Fore.GREEN}              v .   ._, |_  .,
            `7-,--.`._||  / / ,
            /'     `-. `./ / |/_.'
                      |    |//
-    {Fore.YELLOW}Running grid in{Fore.GREEN}  |_    /
-    {Fore.YELLOW}tree mode{Fore.GREEN}        |-   |
+    {Fore.YELLOW}Running Grid in{Fore.GREEN}  |_    /
+    {Fore.YELLOW}Tree Mode{Fore.GREEN}        |-   |
                      |   =|
                      |    |
 --------------------/ ,  . \--------._{Style.RESET_ALL}
+"""
+
+
+compute = f"""{Fore.GREEN}     ____________________________
+    !\_________________________/!\\
+    !!                         !! \\
+    !!                         !!  \\
+    !!                         !!  !
+    !!      {Fore.YELLOW}Running Grid{Fore.GREEN}       !!  !
+    !!     {Fore.YELLOW}in Compute Mode{Fore.GREEN}     !!  !
+    !!                         !!  !
+    !!                         !!  !
+    !!                         !!  /
+    !!_________________________!! /
+    !/_________________________\!/
+       __\_________________/__/!_
+      !_______________________!/
+    ________________________
+   /oooo  oooo  oooo  oooo /!
+  /ooooooooooooooooooooooo/ /
+ /ooooooooooooooooooooooo/ /
+/C=_____________________/_/{Style.RESET_ALL}
 """
