506ff5c87b0ea9797a4e0bbdd586a618cc24eba9
Ben DeCoste <bendecoste@gmail.com>
2018-02-16 13:47:44 -0400

    added a program description

---
+++ grid/lib/strings.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Style
+
+tree = f"""{Fore.GREEN}              v .   ._, |_  .,
+           `-._\/  .  \ /    |/_
+               \\  _\, y | \//
+         _\_.___\\, \\/ -.\||
+           `7-,--.`._||  / / ,
+           /'     `-. `./ / |/_.'
+                     |    |//
+    {Fore.YELLOW}Running grid in{Fore.GREEN}  |_    /
+    {Fore.YELLOW}tree mode{Fore.GREEN}        |-   |
+                     |   =|
+                     |    |
+--------------------/ ,  . \--------._{Style.RESET_ALL}
+"""
