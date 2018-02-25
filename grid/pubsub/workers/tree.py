e0c995a3f5f52fc21fba5a6bd40a475221b5edf5
iamtrask <liamtrask@gmail.com>
2018-02-25 19:49:50 +0000

    Added tree node comments

--- grid/pubsub/workers/tree.py
+++ grid/pubsub/workers/tree.py
@@ -8,35 +8,46 @@ from pathlib import Path
 
 class GridTree(base_worker.GridWorker):
 
+    """
+    This class runs a worker whose purpose is to do the following:
+       - PRIMARY: to facilitate federated learning of a public model on many nodes
+       - PRIMARY: to version control gradients in a branching tree of updates - facilitating the federated learning
+       - SECONDARY: learn about the existence of other nodes on the network - and help others to do so when asked
+    """
+
     def __init__(self):
         super().__init__()
 
+        # prints a pretty picture of a tree
         print(strings.tree)
+
+        # LAUNCH PROCESSES - these are non-blocking and run on their own threads
+
+        # Blocking until this node has found at least one other OpenMined node
+        # This functionality queries https://github.com/OpenMined/BootstrapNodes for Anchor nodes
+        # then asks those nodes for which other OpenMined nodes they know about on the network.
         self.listen_for_openmined_nodes(1)
+
+        # this process serves the purpose of helping other nodes find out about nodes on the network.
+        # if someone queries the "list_worker" channel - it'll send a message directly to the querying node
+        # with a list of the OpenMined nodes of which it is aware.
         self.listen_to_channel(channels.list_workers,self.list_workers)
+
+        # listens to the network and tells other nodes what tasks you'e working on if they ask
         self.listen_to_channel(channels.list_tasks, self.list_tasks)
+
+        # listens for folks who want to add tasks to be trained to the network
         self.listen_to_channel(channels.add_task, self.discovered_tasks)
+
+        # responds to tasks you're specificalyl asked to do
         self.listen_to_channel(channels.list_tasks_callback(self.id), self.discovered_tasks)
+
+        # listens in case someone asks what models i've got
         self.listen_to_channel(channels.list_models, self.list_models)
+
+        # if i'm turning on after having done some previous work - it publishes previous work to the network
         self.publish(channels.list_tasks, commands.list_all)
 
-    def listen_for_models(self, task_name):
-        self.listen_to_channel(channels.add_model(task_name), self.added_model)
-        self.publish(channels.list_models, task_name)
-
-
-    def list_models(self, message):
-        task = message['data']
-        fr = base58.encode(message['from'])
-
-        print(f'listing models {fr} {self.id}')
-
-        if fr == self.id:
-            return
-
-        my_best = utils.best_model_for_task(task)
-        if my_best is not None:
-            self.send_model(task, my_best)
 
     def list_tasks(self, message):
         fr = base58.encode(message['from'])
@@ -53,6 +64,58 @@ class GridTree(base_worker.GridWorker):
         callback_channel = channels.list_tasks_callback(fr)
         print(f'?!?!?!?!?! {callback_channel} {string_list}')
         self.publish(callback_channel, string_list)
+
+
+    ############################### BEGIN PROCESS FUNCTIONS ########################
+
+    def discovered_tasks(self, tasks):
+        print(f'{Fore.WHITE}{Back.BLACK} TASKS {Style.RESET_ALL}')
+        print(f'From\t\t\t\tName\t\t\t\tAddress')
+        print('==================================================================')
+
+        data = json.loads(tasks['data'])
+        fr = tasks['from'] # base58.encode(tasks['from'])
+
+        for task in data:
+            name = task['name']
+            addr = task['address']
+
+            print(f'{fr}\t{name}\t{addr}')
+
+            t = self.api.get_json(addr)
+            if 'data_dir' in t.keys():
+                data_dir = t['data_dir']
+                if os.path.exists(f'data/{data_dir}'):
+                    self.listen_for_models(name)
+                    utils.store_task(name, addr)
+                else:
+                    print(f"DON'T HAVE DATA FOR {name} DATA DIRECTORY: {data_dir}")
+            elif 'adapter' in t.keys():
+                self.listen_for_models(name)
+                utils.store_task(name, addr)
+
+                self.load_adapter(t['adapter'])
+
+
+    def list_models(self, message):
+        task = message['data']
+        fr = base58.encode(message['from'])
+
+        print(f'listing models {fr} {self.id}')
+
+        if fr == self.id:
+            return
+
+        my_best = utils.best_model_for_task(task)
+        if my_best is not None:
+            self.send_model(task, my_best)
+
+
+    def listen_for_models(self, task_name):
+        self.listen_to_channel(channels.add_model(task_name), self.added_model)
+        self.publish(channels.list_models, task_name)
+
+    ############################### BEGIN SUBPROCESSES FUNCTIONS ########################
 
     def train_model(self, model, input, target, name, task_name, task_addr):
         hist = model.fit(
@@ -81,6 +144,7 @@ class GridTree(base_worker.GridWorker):
             utils.save_best_model_for_task(task_name, model)
 
         self.add_model(name, model, parent=task_addr)
+
 
     def added_local_data_model(self, info):
         task_addr = info['task']
@@ -127,6 +191,7 @@ class GridTree(base_worker.GridWorker):
         else:
             print("Can't train your own model so soon!!!!!")
 
+
     def added_adapter_model(self, info):
         task_addr = info['task']
         task_info = self.api.get_json(task_addr)
@@ -146,6 +211,7 @@ class GridTree(base_worker.GridWorker):
         n_test, n_target = grid_adapter.next_input()
         self.train_model(model, n_test, n_target, name, task_name, task_addr)
 
+
     def added_model(self, info):
         info = self.api.get_json(info['data'])
 
@@ -159,36 +225,11 @@ class GridTree(base_worker.GridWorker):
         elif 'adapter' in task_info.keys():
             self.added_adapter_model(info)
 
+
     def load_adapter(self, addr):
         b = self.api.cat(addr)
         utils.ensure_exists(f'{Path.home()}/.openmined/grid/adapters/t.py', b)
         exec(open(f'{Path.home()}/.openmined/grid/adapters/t.py').read())
 
 
-    def discovered_tasks(self, tasks):
-        print(f'{Fore.WHITE}{Back.BLACK} TASKS {Style.RESET_ALL}')
-        print(f'From\t\t\t\tName\t\t\t\tAddress')
-        print('==================================================================')
-
-        data = json.loads(tasks['data'])
-        fr = tasks['from'] # base58.encode(tasks['from'])
-
-        for task in data:
-            name = task['name']
-            addr = task['address']
-
-            print(f'{fr}\t{name}\t{addr}')
-
-            t = self.api.get_json(addr)
-            if 'data_dir' in t.keys():
-                data_dir = t['data_dir']
-                if os.path.exists(f'data/{data_dir}'):
-                    self.listen_for_models(name)
-                    utils.store_task(name, addr)
-                else:
-                    print(f"DON'T HAVE DATA FOR {name} DATA DIRECTORY: {data_dir}")
-            elif 'adapter' in t.keys():
-                self.listen_for_models(name)
-                utils.store_task(name, addr)
-
-                self.load_adapter(t['adapter'])
+    
