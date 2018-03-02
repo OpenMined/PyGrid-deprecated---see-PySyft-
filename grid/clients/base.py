d62de27a1ff4fd5fa5e02c11dc759bba4a4e6608
iamtrask <liamtrask@gmail.com>
2018-03-02 20:20:11 +0000

    raw whoami service seems to be working

--- grid/clients/base.py
+++ grid/clients/base.py
@@ -5,6 +5,8 @@ from ..lib import strings
 from ..services.passively_broadcast_membership import PassivelyBroadcastMembershipService
 from ..services.listen_for_openmined_nodes import ListenForOpenMinedNodesService
 
+import json
+
 class BaseClient(base_worker.GridWorker):
 
     def __init__(self,min_om_nodes=1,known_workers=list(),include_github_known_workers=True):
@@ -22,6 +24,6 @@ class BaseClient(base_worker.GridWorker):
     def get_stats(self,worker_id):
 
         def ret(msg):
-            return json.loads(msg)
-            
-        self.request_response(channel=channels.whoami_listener_callback(worker_id),message=[],response_handler=ret)
+            return json.loads(msg['data'])
+
+        return self.request_response(channel=channels.whoami_listener_callback(worker_id),message=[],response_handler=ret)
