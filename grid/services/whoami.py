4c516996ecfee614e77e0757d18e0438485d95a2
iamtrask <liamtrask@gmail.com>
2018-03-02 19:54:42 +0000

    fixed bug

--- grid/services/whoami.py
+++ grid/services/whoami.py
@@ -24,12 +24,12 @@ class WhoamiService(BaseService):
 
         import psutil
         stats['worker_type'] = self.worker.node_type
-        stats['services_running'] = list(self.worker.services.keys())
+        stats['services_running'] = self.worker.services.keys().tolist()
         stats['id'] = self.worker.id
 
         if('torch_service' in self.worker.services.keys()):
             stats['torch'] = {}
-            stats['torch']['objects'] = self.worker.services['torch_service'].objects.keys()
+            stats['torch']['objects'] = self.worker.services['torch_service'].objects.keys().tolist()
 
         stats['cpu_processor_percent_utilization'] = psutil.cpu_percent()
         stats['cpu_num_cores'] = psutil.cpu_count(logical=False)
