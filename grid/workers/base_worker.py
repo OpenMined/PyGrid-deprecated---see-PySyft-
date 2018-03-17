500ff3d0ca9ebff80351e46006e207896bde9970
iamtrask <liamtrask@gmail.com>
2018-03-17 19:11:01 +0000

    fix typo

--- grid/workers/base_worker.py
+++ grid/workers/base_worker.py
@@ -55,8 +55,10 @@ class GridWorker():
         Note - not all workers are necessarily "compute" workers.
         Some may only be anchors and will ignore any jobs you send them.
         """
-
-        nodes = self.api.pubsub_peers('openmined')['Strings']
+        try:
+            nodes = self.api.pubsub_peers('openmined')['Strings']
+        except:
+            print("Error:" + str(self.api.pubsub_peers('openmined')))
         if (nodes is not None):
             return nodes
         else:
