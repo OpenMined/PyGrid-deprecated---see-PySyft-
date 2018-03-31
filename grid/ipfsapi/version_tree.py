ebf9238e3c742b0ab0e9d21d187a9e0cf845c3a0
Veer Bawa <vbawa89@gmail.com>
2018-03-31 00:46:40 -0700

    Remove references to VersionTreeNode to play nice with subclasses

--- grid/ipfsapi/version_tree.py
+++ grid/ipfsapi/version_tree.py
@@ -22,25 +22,21 @@ class VersionTreeNode:
         this node is the root of a version tree. """
         self.contents = contents
         # Convert empty string to None to minimize typing bugs.
-        self.parent_hash = parent_hash.decode("utf-8") or None
+        self.parent_hash = parent_hash or None
         self.ipfs_client = ipfs_client
 
-    # TODO: Should there just be a singleton IPFS client in the ipfsapi.client
-    # module?  It seems like the current architecture would require
-    # it to either be passed around all over the place or potentially go
-    # through the connection process multiple times, and I don't think
-    # multiple configurations need to be maintained for a given worker node.
     def commit(self, ipfs_client: ipfsapi.Client = None) -> str:
         """ Commits the node to the version tree, and returns the
         UTF-8 multihash representing its IPFS ID"""
         return (ipfs_client or self.ipfs_client).add_bytes(self.to_bytes())
 
-    @staticmethod
-    def get_node_by_hash(multihash: str,
+    @classmethod
+    def get_node_by_hash(cls,
+                         multihash: str,
                          ipfs_client: ipfsapi.Client) -> "VersionTreeNode":
         """ Retrieve and deserialize a VersionTreeNode addressed
         by it's UTF-8 multihash IPFS ID. """
-        return VersionTreeNode.from_bytes(ipfs_client.cat(multihash))
+        return cls.from_bytes(ipfs_client.cat(multihash))
 
     def get_with_ancestors(
             self,
@@ -50,20 +46,21 @@ class VersionTreeNode:
         yield self
         parent_hash = self.parent_hash
         while parent_hash is not None:
-            parent_node = VersionTreeNode.get_node_by_hash(
+            parent_node = self.get_node_by_hash(
                 parent_hash,
                 (ipfs_client or self.ipfs_client))
             parent_hash = parent_node.parent_hash
             yield parent_node
 
-    @staticmethod
+    @classmethod
     def get_node_with_ancestors_by_hash(
+            cls,
             multihash: str,
             ipfs_client: ipfsapi.Client) -> Iterator["VersionTreeNode"]:
         """ Convenience method to get an iterator of the node identified by the
         provided UTF-8 IPFS multihash, along with all of its ancestors, in
         that order."""
-        return VersionTreeNode.get_node_by_hash(
+        return cls.get_node_by_hash(
             multihash, ipfs_client).get_with_ancestors(ipfs_client)
 
     def to_bytes(self) -> bytes:
@@ -72,14 +69,13 @@ class VersionTreeNode:
         parent_hash_bytes = self.parent_hash.encode("utf-8") if \
             self.parent_hash else \
             b""
-        return VersionTreeNode.DELIMITER.join(
-            (self.contents, parent_hash_bytes)
-        )
+        return self.DELIMITER.join((self.contents, parent_hash_bytes))
 
-    @staticmethod
-    def from_bytes(b: bytes) -> "VersionTreeNode":
+    @classmethod
+    def from_bytes(cls, b: bytes) -> "VersionTreeNode":
         """ In case the contents section happens to contain the DELIMITER
         string, only splits on the final occurrence of DELIMITER. The
         multihash is hexadecimal, so it won't contain the non-hex DELIMITER."""
-        return VersionTreeNode(*b.rsplit(VersionTreeNode.DELIMITER,
-                                         maxsplit=1))
+        contents, parent_hash_bytes = b.rsplit(cls.DELIMITER, maxsplit=1)
+        return cls(contents, parent_hash_bytes.decode("utf-8"))
+
