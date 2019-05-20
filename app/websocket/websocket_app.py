7296188f1f9d9239d0e688b30f5975406de66a74
IonesioJunior <ionesiojr@gmail.com>
2019-05-20 01:17:59 -0300

    Change websocket application

--- app/websocket/websocket_app.py
+++ app/websocket/websocket_app.py
@@ -1,41 +1,47 @@
-import binascii
-import json
-import os
-
-import redis
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 import syft as sy
 import torch as th
-from gevent import pywsgi
-from flask import Flask, render_template
-import socketio
+import redis
+import binascii
+import os
 
+hook = sy.TorchHook(th)
+
+# Set up REDIS URL
 try:
     db = redis.from_url(os.environ['REDISCLOUD_URL'])
 except:
     db = redis.from_url('redis://localhost:6379')
 
 
-hook = sy.TorchHook(th)
-async_mode = 'gevent'
-sio = socketio.Server(async_mode=async_mode)
+# Initialize app/socket vars
 app = Flask(__name__)
-app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
+app.config['SECRET_KEY'] = 'justasecretkeythatishouldputhere'
+socketio = SocketIO(app, async_mode="eventlet")
+CORS(app)
 
 
 def _maybe_create_worker(worker_name: str = 'worker', virtual_worker_id: str = 'grid'):
     worker = db.get(worker_name)
-
     if worker is None:
         worker = sy.VirtualWorker(hook, virtual_worker_id, auto_add=False)
-        print("\t \nCREATING NEW WORKER!!")
     else:
         worker = sy.serde.deserialize(worker)
-        print("\t \nFOUND OLD WORKER!! " + str(worker._objects.keys()))
     return worker
 
 def _store_worker(worker, worker_name: str = 'worker'):
     db.set(worker_name, sy.serde.serialize(worker, force_full_simplification=True))
 
+
+def _request_message(worker, request):
+    print("Message Type: ", type(request))
+    message = request['message']
+    message = binascii.unhexlify(message[2:-1])
+    response = worker._recv_msg(message)
+    response = str(binascii.hexlify(response))
+    return response
 
 @app.route('/')
 def hello_world():
@@ -43,51 +49,30 @@ def hello_world():
     db.set('del_ctr', 0)
     return 'Websocket Howdy %s!' % str(name)
 
-
-@sio.on("/identity/")
+@socketio.on("/identity")
 def is_this_an_opengrid_node():
     """This exists because in the automation scripts which deploy nodes,
     there's an edge case where the 'node already exists' but sometimes it
     can be an app that does something totally different. So we want to have
     some endpoint which just casually identifies this server as an OpenGrid
     server."""
-    sio.emit('/identity', "Websocket OpenGrid")
+    socketio.emit('/identity', "Websocket OpenGrid")
 
-
-@sio.on('/cmd/')
-def cmd(sid, message):
+@socketio.on('/cmd')
+def cmd(message):
     try:
         worker = _maybe_create_worker("worker", "grid")
 
         worker.verbose = True
         sy.torch.hook.local_worker.add_worker(worker)
 
-        response = worker._recv_msg(message)
-
-        print("\t NEW WORKER STATE:" + str(worker._objects.keys()) + "\n\n")
+        response = _request_message(worker, message)
 
         _store_worker(worker, "worker")
 
-        sio.emit("/cmd/", response)
+        socketio.emit("/cmd", response)
     except Exception as e:
-        print("Error: ", e)
-        sio.emit("/cmd/", str(e))
-
-
+        socketio.emit("/cmd", str(e))
 
 if __name__ == '__main__':
-    if sio.async_mode == 'gevent':
-        # deploy with gevent
-        from gevent import pywsgi
-        try:
-            from geventwebsocket.handler import WebSocketHandler
-            websocket = True
-        except ImportError:
-            websocket = False
-        if websocket:
-            pywsgi.WSGIServer(('', 5000),
-                              handler_class=WebSocketHandler).serve_forever()
-        else:
-            pywsgi.WSGIServer(('', 5000), app).serve_forever()
-    else:
-        print('Unknown async_mode: ' + sio.async_mode)
+    socketio.run(app)
