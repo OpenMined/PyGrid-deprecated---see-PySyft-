f29291b1c41669fd41a130c12f80616986cc8f63
Jon Lebensold <jon@lebensold.ca>
2019-06-03 15:28:40 -0400

    removing dead code

--- test/test_flask_integration.py
+++ test/test_flask_integration.py
@@ -3,21 +3,8 @@ import tempfile
 
 import pytest
 
-#from app import app
 
 
 @pytest.fixture
 def client():
     return
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
-
-    client = flaskr.app.test_client()
-
-    with flaskr.app.app_context():
-        flaskr.init_db()
-
-    yield client
-
-    os.close(db_fd)
-    os.unlink(flaskr.app.config['DATABASE'])
