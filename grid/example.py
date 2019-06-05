84ad7768f6222ba3a9fd551a7864d7adbc20f64c
Jon Lebensold <jon@lebensold.ca>
2019-06-05 10:25:48 -0400

    unnecessary for now

--- grid/example.py
+++ grid/example.py
@@ -2,6 +2,6 @@ from app import db
 from app.models import Tensor
 
 
-@app.shell_context_processor
+# @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "Tensor": Tensor}
