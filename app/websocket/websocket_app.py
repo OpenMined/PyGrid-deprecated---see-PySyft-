22f82b27a9a3823ac703d215fcc3598e4420a80d
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-08-18 20:36:18 -0300

    Create port argument for websocket_app

--- app/websocket/websocket_app.py
+++ app/websocket/websocket_app.py
@@ -2,7 +2,15 @@
 from app import create_app, socketio
 import sys
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--port", default=8765, type=str, help="port where websocket app will be served."
+)
+
 app = create_app(debug=True)
 
 if __name__ == "__main__":
-    socketio.run(app)
+    args = parser.parse_args()
+    socketio.run(app, port=args.port)
