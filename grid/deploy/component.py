1cefb689452a59e1396469f48e8511620cce40b5
IonesioJunior <ionesiojr@gmail.com>
2019-08-21 07:25:44 -0300

    FIX undefined var

--- grid/deploy/component.py
+++ grid/deploy/component.py
@@ -45,7 +45,7 @@ class BaseDeployment(ABC):
         if verbose:
             sys.stdout.write("\tChecking for " + str(lib) + " dependency...")
         output = gr_utils.execute_command(lib)
-        if check not in o:
+        if check not in ouput:
             raise RuntimeError(error_msg)
         if verbose:
             print("DONE!")
