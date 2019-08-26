fd8bed4cfe622a66b6ba633fca468be85b292613
IonesioJunior <ionesiojr@gmail.com>
2019-08-26 01:10:49 -0300

    Snapshot method performance optmization - Before, for every command, we deleted ALL items registered on database and added ALL items again. - Now, for every command, we add/remove the difference between current and last snapshot.

--- app/websocket/app/main/db_module/utils.py
+++ app/websocket/app/main/db_module/utils.py
@@ -2,16 +2,23 @@ from .models import Worker as WorkerMDL
 from .models import WorkerObject
 from .models import db
 
+last_snapshot_keys = set()
+
 
 def snapshot(worker):
     """ Persist workers to our persistence layer """
-    db.session.query(WorkerObject).filter_by(worker_id=worker.id).delete()
+    global last_snapshot_keys
+    current_keys = set(worker._objects.keys())
+    new_keys = current_keys - last_snapshot_keys
+
     objects = [
-        WorkerObject(worker_id=worker.id, object=obj, id=key)
-        for key, obj in worker._objects.items()
+        WorkerObject(worker_id=worker.id, object=worker._objects[key], id=key)
+        for key in new_keys
     ]
+
     result = db.session.add_all(objects)
     db.session.commit()
+    last_snapshot_keys = current_keys
 
 
 def recover_objects(hook):
@@ -19,11 +26,13 @@ def recover_objects(hook):
     worker = hook.local_worker
     worker_mdl = WorkerMDL.query.filter_by(id=worker.id).first()
     if worker_mdl:
+        global last_snapshot_keys
         objs = db.session.query(WorkerObject).filter_by(worker_id=worker.id).all()
         obj_dict = {}
         for obj in objs:
             obj_dict[obj.id] = obj.object
         worker._objects = obj_dict
+        last_snapshot_keys = set(obj_dict.keys())
     else:
         worker_mdl = WorkerMDL(id=worker.id)
         db.session.add(worker_mdl)
