705a9cfa1c3bee4c9ddad82ca5c68e21ddd734c0
Tallal Javed <Tallal.javed@outlook.com>
2019-08-27 17:12:15 +0500

    Update utils.py

--- grid/utils.py
+++ grid/utils.py
@@ -2,12 +2,9 @@
 import os
 
 def execute_command(command):
-    """
-        Executes the given command using the os inherent shell.
+    """Executes the given command using the os inherent shell.
 
-        Parameters
-        ----------
-        command : str
-            The command to execute
+        Args:
+            command (str): The command to execute
     """
     return os.popen(command).read()
