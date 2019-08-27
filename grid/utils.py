4f64dee2e9285162407d7d897f502988563365bd
quisher <tallal.javed@outlook.com>
2019-08-27 16:40:38 +0500

    __init__.py: - Fixed a typo in execute_command(command) - Added documentation to launch_on_heroku function - Removed unessacery if from launch_on_heroku
    utils.py:
    - Added documentation

--- grid/utils.py
+++ grid/utils.py
@@ -1,6 +1,13 @@
 """Utility functions."""
 import os
 
-
 def execute_command(command):
+    """
+        Executes the given command using the os inherent shell.
+
+        Parameters
+        ----------
+        command : str
+            The command to execute
+    """
     return os.popen(command).read()
