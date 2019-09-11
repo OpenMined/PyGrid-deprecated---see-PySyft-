f0e65a245c0ead3d887f7b86e1f775ea0c691b0d
Marianne Linhares <mariannelinharesm@gmail.com>
2019-09-11 15:26:41 +0100

    Fix import order and typos

--- app/websocket/websocket_app.py
+++ app/websocket/websocket_app.py
@@ -2,13 +2,14 @@
 """
     Grid Node is a Socket/HTTP server used to manage / compute data remotely.
 """
-
-from app import create_app, socketio
-import sys
-import requests
 import json
 import os
+import requests
+import sys
+
 import argparse
+
+from app import create_app, socketio
 
 
 parser = argparse.ArgumentParser(description="Run Grid Node application.")
@@ -16,7 +17,7 @@ parser = argparse.ArgumentParser(description="Run Grid Node application.")
 parser.add_argument(
     "--id",
     type=str,
-    help="Grid node ID, e.g. --id=alice. Default is os.environ.get('WS_ID', None).",
+    help="Grid node ID, e.g. --id=alice. Default is os.environ.get('GRID_WS_ID', None).",
     default=os.environ.get("GRID_WS_ID", None),
 )
 
@@ -24,14 +25,14 @@ parser.add_argument(
     "--port",
     "-p",
     type=int,
-    help="Port number of the socket.io server, e.g. --port=8777. Default is os.environ.get('WS_PORT', None).",
+    help="Port number of the socket.io server, e.g. --port=8777. Default is os.environ.get('GRID_WS_PORT', None).",
     default=os.environ.get("GRID_WS_PORT", None),
 )
 
 parser.add_argument(
     "--host",
     type=str,
-    help="Grid node host, e.g. --host=0.0.0.0. Default is os.environ.get('WS_HOST','0.0.0.0').",
+    help="Grid node host, e.g. --host=0.0.0.0. Default is os.environ.get('GRID_WS_HOST','0.0.0.0').",
     default=os.environ.get("GRID_WS_HOST", "0.0.0.0"),
 )
 
@@ -46,7 +47,7 @@ parser.add_argument(
     "--use_test_config",
     dest="use_test_config",
     action="store_true",
-    help="If this flag is used a SQLAlchemy DB URI is generated to use a local",
+    help="If this flag is used a SQLAlchemy DB URI is generated to use a local db.",
 )
 
 parser.set_defaults(use_test_config=False)
