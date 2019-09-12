c8ec0b9835b2c1a807e4e37a397e8409c6722605
IonesioJunior <ionesiojr@gmail.com>
2019-09-12 19:21:56 -0300

    ADD comment about eventlet money_patch

--- app/websocket/app/__init__.py
+++ app/websocket/app/__init__.py
@@ -9,9 +9,10 @@ import os
 # and ping timeout
 PING_INTERVAL = 10000000
 PING_TIMEOUT = 5000
+
 from eventlet import monkey_patch
 
-monkey_patch()
+monkey_patch()  # Turn every blocking module used by this app as much as asynchronous as possible
 
 socketio = SocketIO(
     async_mode="eventlet", ping_interval=PING_INTERVAL, ping_timeout=PING_TIMEOUT
