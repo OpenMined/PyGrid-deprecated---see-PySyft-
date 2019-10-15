fbc6d45a026ee7766b2eb3fa4cedf35dfd13a4be
IonesioJunior <ionesiojr@gmail.com>
2019-10-15 06:48:08 -0300

    ADD default admin infos

--- app/websocket/app/main/auth/session_repository.py
+++ app/websocket/app/main/auth/session_repository.py
@@ -1,12 +1,17 @@
 from grid.auth.config import read_authentication_configs
-from grid.auth import AUTH_MODELS
+from grid.auth import AUTH_MODELS, UserAuthentication
 from .user_session import UserSession
 
 
-class SessionsRepository:
+class SessionsRepository(object):
+    DEFAULT_MANAGER_USER = "admin"
+    DEFAULT_MANAGER_PASSWORD = "admin"
+
     def __init__(self):
         self.users = dict()
         self.users_id_dict = dict()
+        self._admin = UserSession(self.__load_node_manager())
+        self.save_session(self._admin, self._admin.username())
 
     def save_session(self, user, key):
         self.users[key] = user
@@ -26,6 +31,12 @@ class SessionsRepository:
         if session_object:
             if session_object.authenticate(payload):
                 return session_object
+
+    def __load_node_manager(self):
+        return UserAuthentication(
+            SessionsRepository.DEFAULT_MANAGER_USER,
+            SessionsRepository.DEFAULT_MANAGER_PASSWORD,
+        )
 
     def __load_preset_credentials(self):
         for cred in read_authentication_configs():
