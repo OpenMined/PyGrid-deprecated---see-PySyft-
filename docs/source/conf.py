0a88a43f81890b6ca8ccd1ce7ae7a68097e4c6b8
Benardi <benardinunes@gmail.com>
2019-11-02 16:32:15 -0300

    Conform code to black standards

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,17 +83,17 @@ html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "collapse_navigation" : False
-}
+html_theme_options = {"collapse_navigation": False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+
 def setup(app):
     app.add_stylesheet("css/theme.css")
+
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -184,8 +184,8 @@ epub_exclude_files = ["search.html"]
 
 # AutoApi
 autoapi_keep_files = True
-autoapi_root = 'api'
-autoapi_dirs = ['../../grid']
+autoapi_root = "api"
+autoapi_dirs = ["../../grid"]
 
 
 # Napoleon settings
