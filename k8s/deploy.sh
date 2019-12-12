6d5be852b6b2e6396e69b1e18ba68a48f61d18dd
Ramjee Ganti <code@ramjeeganti.com>
2019-12-12 17:01:31 +0530

    Fixed the deploy to proper shape

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,34 +1,83 @@
 #!/bin/bash
 
-echo Deploy new versions. Argument: $1
-date
+deploy() {
+    # Params: $input_target $selected_cluster
+    echo "Deploying to cluster "$2" on target "$1
 
-LOCAL_CLUSTER="minikube"
-TEST_CLUSTER="gke_foobar-123456_europe-north1-b_xyzzy-test-cluster"
-PROD_CLUSTER="gke_foobar-123456_europe-north1-b_xyzzy-prod-cluster"
+    # 1. Make the Local cluster active
+    kubectl config set current-context $2
+    # 2. Create/update Cluster
+    kustomize build overlays/$1/gateway/ | kubectl apply -f -
+    kustomize build overlays/$1/node-alice/ | kubectl apply -f -
+}
 
-SELECTED_CLUSTER=$LOCAL_CLUSTER
+usage() {
+cat << EOF
+Usage: ./deploy.sh -t <local|test|prod> [-ch]
+Deploy PyGrid Gateway, Application Node to Kubernetes.
 
-if [[ $1 = "prod" ]]
+-h, Display help
+-t, Specify the target environment (local|test|prod) to be deployed to
+-c, Speicfy the cluster name
+
+EOF
+}
+
+function in_array {
+  ARRAY=$2
+  for e in ${ARRAY[*]}
+  do
+    if [[ "$e" == "$1" ]]
+    then
+      return 0
+    fi
+  done
+  return 1
+}
+
+# Array of supported targets
+valid_targets=(local prod test)
+
+input_target="local"
+cluster_name=""
+
+while getopts ":ht:c:" arg
+do
+    case $arg in
+        t)
+            input_target=${OPTARG}
+            ;;
+        c)
+            cluster_name=${OPTARG}
+            ;;
+        h | *)
+            usage
+            exit 0
+            ;;
+        \? )
+            usage
+            exit 0
+            ;;
+    esac
+done
+
+if in_array $input_target "${valid_targets[*]}"
 then
-    SELECTED_CLUSTER=$PROD_CLUSTER
-fi
-
-if [[ $1 = "test" ]]
-then
-    SELECTED_CLUSTER=$TEST_CLUSTER
-fi
-
-echo "Deploy to "$SELECTED_CLUSTER
-
-# 1. Make the Local cluster active
-kubectl config set current-context $SELECTED_CLUSTER
-# 2. Create/update Cluster
-kustomize build overlays/$1/gateway/ | kubectl apply -f -
-kustomize build overlays/$1/node-alice/ | kubectl apply -f -
-
-# Starting minikube tunnel so that the loadbalancer service type can work.
-if [[ $1 == "local" && $SELECTED_CLUSTER == "minikube" ]]
-then
-    minikube tunnel &
+    if [[ $input_target == "local" && $cluster_name != "minikube" ]]
+    then
+        cluster_name="minikube"
+        echo 'Only minikube is supported on local. Going ahead with the same'
+    fi
+    if [[ $input_target == "prod" || $input_target == "test"  ]]
+    then
+        if [[ -z "$cluster_name" ]]
+        then
+            usage
+            exit 0
+        fi
+    fi
+    deploy $input_target $cluster_name
+else
+    usage
+    exit 0
 fi
