e962aa5f9761ade2be15679fd5a4f7a24d727c18
Héricles Emanuel <hericles.me@gmail.com>
2020-02-02 11:14:38 -0300

    update gateway app runner on conftest and fix socket ping endpoint

--- gateway/app/main/events/control_events.py
+++ gateway/app/main/events/control_events.py
@@ -2,6 +2,6 @@ import json
 from ..codes import RESPONSE_MSG
 
 
-def socket_ping(message: dict) -> str:
+def socket_ping(message: dict, socket) -> str:
     """ Ping request to check node's health state. """
     return json.dumps({RESPONSE_MSG.ALIVE: "True"})
