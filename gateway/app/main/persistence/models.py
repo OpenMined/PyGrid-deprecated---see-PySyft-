5d9b8b26947aa120cad4d1442ed48ea727bd6fff
Ionésio Junior <ionesiojr@gmail.com>
2020-02-20 17:37:12 -0300

    Update models.py
    ADD tablenames / fix docstrings

--- gateway/app/main/persistence/models.py
+++ gateway/app/main/persistence/models.py
@@ -1,3 +1,4 @@
+import json
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,15 +7,15 @@ db = SQLAlchemy()
 class Model(db.Model):
     """ Model table that represents the AI Models.
         Columns:
-            id (primary_key) : Model's id, used to recover stored model.
-            version (string) : Model version.
+            id (String, primary_key) : Model's id, used to recover stored model.
+            version (String) : Model version.
             checkpoints (list) : Model Checkpoints.
     """
 
     __tablename__ = "__model__"
 
-    id = db.Column(db.String(64), primary_key=True)
-    version = db.Column(db.String(64))
+    id = db.Column(db.String(), primary_key=True)
+    version = db.Column(db.String())
     # TODO:
     # checkpoints = list(ModelCheckpoints)
 
@@ -25,33 +26,41 @@ class Model(db.Model):
 class ModelCheckPoint(db.Model):
     """ Model's save points.
         Columns:
-            id: Checkpoint ID.
-            values: Value of the model at a given checkpoint.
+            id (String, primary_key): Checkpoint ID.
+            values (Binary): Value of the model at a given checkpoint.
     """
 
     __tablename__ = "__model_checkpoint__"
 
-    id = db.Column(db.String(64), primary_key=True)
-    # TODO
-    # values = TENSOR
+    id = db.Column(db.String(), primary_key=True)
+    values = db.Column(db.LargeBinary)
+    """
+    @property
+    def object(self):
+        return sy.serde.deserialize(self.values)
+
+    @object.setter
+    def object(self):
+        self.data = sy.serde.serialize(self.values)
+    """
 
     def __str__(self):
-        return f"<CheckPoint id: {self.id}>"
+        return f"<CheckPoint id: {self.id} , values: {self.data}>"
 
 
 class Plan(db.Model):
     """ Plan table that represents Syft Plans.
         Columns:
-            id: Plan ID.
-            value: String  (List of operations)
-            value_ts: String (TorchScript)
+            id (String): Plan ID.
+            value (String): String  (List of operations)
+            value_ts (String): String (TorchScript)
     """
 
     __tablename__ = "__plan__"
 
-    id = db.Column(db.String(64), primary_key=True)
-    value = db.Column(db.String(64))
-    value_ts = db.Column(db.String(64))
+    id = db.Column(db.String(), primary_key=True)
+    value = db.Column(db.String())
+    value_ts = db.Column(db.String())
 
     def __str__(self):
         return (
@@ -62,38 +71,49 @@ class Plan(db.Model):
 class Protocol(db.Model):
     """ Protocol table that represents Syft Protocols.
         Columns:
-            id: Protocol ID.
+            id (String, Primary Key): Protocol ID.
             value: String  (List of operations)
             value_ts: String (TorchScript)
     """
 
     __tablename__ = "__protocol__"
 
-    id = db.Column(db.String(64), primary_key=True)
-    value = db.Column(db.String(64))
-    value_ts = db.Column(db.String(64))
+    id = db.Column(db.String(), primary_key=True)
+    value = db.Column(db.String())
+    value_ts = db.Column(db.String())
 
     def __str__(self):
-        return (
-            f"<Plan id: {self.id}, values: {self.value}, torchscript: {self.value_ts}>"
-        )
+        return f"<Protocol id: {self.id}, values: {self.value}, torchscript: {self.value_ts}>"
 
 
 class Config(db.Model):
     """ Configs table.
         Columns:
-            id: Config ID.
-            value: Dictionary
+            id (String, Primary Key): Config ID.
+            value (String): Dictionary
     """
 
-    id = db.Column(db.String(64), primary_key=True)
-    # TODO
-    # configs = Dict
+    __tablename__ = "__config__"
+
+    id = db.Column(db.String(), primary_key=True)
+    config = db.Column(db.String())
+
+    @property
+    def object(self):
+        # Convert Python Dict data structure to String
+        return json.dumps(self.config)
+
+    @object.setter
+    def object(self):
+        # Convert Python String to Dict data tructure
+        self.config = json.loads(self.config)
+
+    def __str__(self):
+        return f"<Config id: {self.id} , configs: {self.config}>"
 
 
 class Cycle(db.Model):
     """ Cycle table.
-
         Columns:
             id (String):
             start (TIME): Start time.
@@ -101,7 +121,17 @@ class Cycle(db.Model):
             worker_cycles:
     """
 
-    id = db.Column(db.String(64), primary_key=True)
+    __tablename__ = "__cycle__"
+
+    id = db.Column(db.String(), primary_key=True)
+    start = db.Column(db.DateTime())
+    end = db.Column(db.DateTime())
+    sequence = db.Column(db.BigInteger())
+    # TODO:
+    # worker_cycles =
+
+    def __str__(self):
+        return f"< Cycle id : {self.id}, start: {self.start}, end: {self.end}>"
 
 
 class WorkerCycle(db.Model):
@@ -114,11 +144,13 @@ class WorkerCycle(db.Model):
             request_key (String): unique token that permits downloading specific Plans, Protocols, etc.
     """
 
-    id = db.Column(db.String(64), primary_key=True)
-    fl_process_id = db.Column(db.String(64))
-    cycle_id = db.Column(db.String(64))
-    worker_id = db.Column(db.String(64))
-    request_key = db.Column(db.String(64))
+    __tablename__ = "__worker_cycle__"
+
+    id = db.Column(db.String(), primary_key=True)
+    fl_process_id = db.Column(db.String())
+    cycle_id = db.Column(db.String())
+    worker_id = db.Column(db.String())
+    request_key = db.Column(db.String())
 
     def __str__(self):
         f"<WorkerCycle id: {self.id}, fl_process: {self.fl_process_id}, cycle: {self.cycle_id}, worker: {self.worker}, request_key: {self.request_key}>"
@@ -135,11 +167,13 @@ class Worker(db.Model):
             worker_cycles [WorkerCycles] : List of cycles used by this worker.
     """
 
-    id = db.Column(db.String(64), primary_key=True)
-    format_preference = db.Column(db.String(64))
+    __tablename__ = "__worker__"
+
+    id = db.Column(db.String(), primary_key=True)
+    format_preference = db.Column(db.String())
     ping = db.Column(db.BigInteger)
-    avg_download = db.Column(db.String(64))
-    avg_upload = db.Column(db.String(64))
+    avg_download = db.Column(db.BigInteger)
+    avg_upload = db.Column(db.BigInteger)
     # TODO
     # Worker Cycles (Worker)
 
@@ -156,14 +190,16 @@ class FLProcess(db.Model):
                 training_plan: Plan
                 validation-plan: Plan
             ]
-            protocols (Protocol) :
             client_config (Config):
+            protocols (Protocol) :
             server_config (Config):
             model (Model): 
             cycles [Cycles]:
     """
 
-    id = db.Column(db.String(64), primary_key=True)
+    __tablename__ = "__fl_process__"
+
+    id = db.Column(db.String(), primary_key=True)
 
     def __str__(self):
         return f"<FederatedLearningProcess id : {self.id}>"
@@ -179,8 +215,8 @@ class GridNodes(db.Model):
 
     __tablename__ = "__gridnode__"
 
-    id = db.Column(db.String(64), primary_key=True)
-    address = db.Column(db.String(64))
+    id = db.Column(db.String(), primary_key=True)
+    address = db.Column(db.String())
 
     def __str__(self):
         return f"< Grid Node {self.id} : {self.address}>"
