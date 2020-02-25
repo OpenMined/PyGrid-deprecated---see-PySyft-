0cc9062334922f5e11530a6e8f46af5967ca2b47
Ionésio Junior <ionesiojr@gmail.com>
2020-02-25 09:43:24 -0300

    Update __init__.py
    replace import: persistence -> storage

--- gateway/app/main/__init__.py
+++ gateway/app/main/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 main = Blueprint("main", __name__)
 ws = Blueprint(r"ws", __name__)
 
-from .persistence.models import db
+from .storage.models import db
 
 from . import routes, events
