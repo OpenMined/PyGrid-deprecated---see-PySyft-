cf474a9e971104416045d3186210069e63f9cf98
Ionésio Junior <ionesiojr@gmail.com>
2020-02-27 10:22:17 -0300

    Update controller.py
    Rename dict (workers -> cache) / Add storage attribute

--- gateway/app/main/auth/controller.py
+++ gateway/app/main/auth/controller.py
@@ -1,11 +1,13 @@
 from .worker import Worker
-
+from ..storage.db_manager import Warehouse
+from ..storage import models
 
 class WorkerController:
     """ This class implements controller design pattern over the workers."""
 
     def __init__(self):
-        self.workers = {}
+        self.cache = {}
+        self._storage = Warehouse(models.Worker)
 
     def create_worker(self, worker_id: str):
         """ Register a new worker
@@ -14,16 +16,21 @@ class WorkerController:
             Returns:
                 worker: a Worker instance.
         """
+        if self._storage.contains(worker_id)
+            return self._storage.query(id=worker_id)
+        
+        self._storage.register({"id": worker_id})
         worker = Worker(worker_id)
-        self.workers[worker.worker_id] = worker
-        return self.workers[worker.worker_id]
+        self.cache[worker.worker_id] = worker
+        return self.cache[worker.worker_id]
 
     def delete_worker(self, worker_id):
         """ Remove a registered worker.
             Args:
                 worker_id: Id used identify the desired worker. 
         """
-        del self.workers[worker_id]
+        self._storage.delete(id=worker_id)
+        del self.cache[worker_id]
 
     def get_worker(self, worker_id):
         """ Retrieve the desired worker.
@@ -32,4 +39,10 @@ class WorkerController:
             Returns:
                 worker: worker Instance or None if it wasn't found.
         """
-        return self.workers.get(worker_id, None)
+        _worker = self.cache.get(worker_id, None)
+        
+        if not _worker:
+            _worker = self._storage.query(id=worker_id)
+        
+        return _worker
+
