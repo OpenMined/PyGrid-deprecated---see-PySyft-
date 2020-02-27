63ddd6861702ed895b546d8008c8c4e2eb95928f
Ionésio Junior <ionesiojr@gmail.com>
2020-02-27 16:09:11 -0300

    Update controller.py
    Add an worker database interface

--- gateway/app/main/auth/controller.py
+++ gateway/app/main/auth/controller.py
@@ -1,5 +1,5 @@
 from .worker import Worker
-from ..storage.db_manager import Warehouse
+from ..storage.warehouse import Warehouse
 from ..storage import models
 
 
@@ -7,42 +7,34 @@ class WorkerController:
     """ This class implements controller design pattern over the workers."""
 
     def __init__(self):
-        self.cache = {}
-        self._storage = Warehouse(models.Worker)
+        self._workers = Warehouse(models.Worker)
 
-    def create_worker(self, worker_id: str):
+    def create(self, worker_id: str):
         """ Register a new worker
             Args:
                 worker_id: id used to identify the new worker.
             Returns:
                 worker: a Worker instance.
         """
-        if self._storage.contains(worker_id):
-            return self._storage.query(id=worker_id)
+        new_worker = self._workers.register(id=worker_id)
+        return new_worker
 
-        self._storage.register({"id": worker_id})
-        worker = Worker(worker_id)
-        self.cache[worker.worker_id] = worker
-        return self.cache[worker.worker_id]
-
-    def delete_worker(self, worker_id):
+    def delete(self, **kwargs):
         """ Remove a registered worker.
             Args:
                 worker_id: Id used identify the desired worker. 
         """
-        self._storage.delete(id=worker_id)
-        del self.cache[worker_id]
+        self._workers.delete(**kwargs)
 
-    def get_worker(self, worker_id):
+    def get(self, **kwargs):
         """ Retrieve the desired worker.
             Args:
                 worker_id: Id used to identify the desired worker.
             Returns:
                 worker: worker Instance or None if it wasn't found.
         """
-        _worker = self.cache.get(worker_id, None)
+        return self._workers.query(**kwargs)
 
-        if not _worker:
-            _worker = self._storage.query(id=worker_id)
-
-        return _worker
+    def update(self, worker):
+        """ Update Workers Attributes. """
+        return self._workers.update()
