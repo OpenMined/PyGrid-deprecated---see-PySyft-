69afc99684c36100d258d94fb8c841e9ee2ac022
jmaunon <juanmixp@gmail.com>
2020-06-06 16:16:59 +0200

    Minor updates

--- grid/__main__.py
+++ grid/__main__.py
@@ -7,15 +7,18 @@ import sys
 
 import argparse
 
-from .app import create_app
+from . import create_app
 
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 
 parser = argparse.ArgumentParser(description="Run Grid Gatway application.")
+parser._action_groups.pop()
 
+required = parser.add_argument_group("required  arguments")
+optional = parser.add_argument_group("optional arguments")
 
-parser.add_argument(
+required.add_argument(
     "--port",
     "-p",
     type=int,
@@ -23,25 +26,25 @@ parser.add_argument(
     default=os.environ.get("GRID_GATEWAY_PORT", None),
 )
 
-parser.add_argument(
+optional.add_argument(
     "--host",
     type=str,
     help="Grid node host, e.g. --host=0.0.0.0. Default is os.environ.get('GRID_GATEWAY_HOST','0.0.0.0').",
     default=os.environ.get("GRID_GATEWAY_HOST", "0.0.0.0"),
 )
 
-parser.add_argument(
+optional.add_argument(
     "--num_replicas",
     type=int,
     help="Number of replicas to provide fault tolerance to model hosting. If None no replica is used (aka num_replicas = 1). Default is os.environ.get('NUM_REPLICAS', None).",
     default=os.environ.get("NUM_REPLICAS", None),
 )
 
-parser.add_argument(
+optional.add_argument(
     "--start_local_db",
     dest="start_local_db",
     action="store_true",
-    help="If this flag is used a SQLAlchemy DB URI is generated to use a local db.",
+    help="If this flag is used a SQLAlchemy DB URI is generated to use a local db. If not provide via DATABASE_URL environment variable.",
 )
 
 parser.set_defaults(use_test_config=False)
