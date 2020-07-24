8cbe645cd87d853e69cf59ba74113e5ec384edd8
Ionesio Junior <ionesiojr@gmail.com>
2020-07-24 08:07:19 -0300

    Fix/Reorganize model centric unit tests

---
+++ apps/node/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from grid_node.app.main import db, BaseModel
+from grid_node.app import create_app
+
+
+@pytest.fixture(scope="function", autouse=True)
+def app():
+    db_path = "sqlite:///:memory:"
+    return create_app(node_id="Maria", test_config={"SQLALCHEMY_DATABASE_URI": db_path})
+
+
+@pytest.fixture(scope="function")
+def database(app):
+    test_db = db
+    test_db.init_app(app)
+    BaseModel.set_session(test_db.session)
+    app.app_context().push()
+    test_db.create_all()
+    return test_db
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
