1ee4a05e408c615780bb9f8b7f288038856f112f
AmrMKayid <amrmkayid@gmail.com>
2020-08-12 00:05:23 +0200

    Update variables.tf

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   type    = string
 }
 
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "subnet_cidr_block" {
+  default = "10.0.1.0/24"
+  type = string
+}
+
 variable "amis" {
   type = map(string)
   default = {
