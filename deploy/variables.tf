509e1b604a075a2fb85b0dfd9ded442a02bb0d23
AmrMKayid <amrmkayid@gmail.com>
2020-08-14 14:34:31 +0200

    Add vm_names map for network and nodes deployment

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
-  default = "eu-west-2" # London
+  default = "us-east-1" # London
   type    = string
 }
 
 variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
-  type = string
+  type    = string
 }
 
 variable "subnet_cidr_block" {
   default = "10.0.1.0/24"
-  type = string
+  type    = string
 }
 
 variable "amis" {
@@ -19,5 +19,16 @@ variable "amis" {
     "eu-west-2"  = "ami-00f6a0c18edb19300" # London, Ubuntu AMI
     "ap-south-1" = "ami-02d55cb47e83a99a0" # Mumbai, Ubuntu AMI
     "us-east-1"  = "ami-0ac80df6eff0e70b5" # N. Virginia, Ubuntu AMI
+  }
+}
+
+variable "vm_names" {
+  type = map(string)
+  default = {
+    "OpenMined_network" = "7000",
+    "bob_node"          = "5000",
+    "alice_node"        = "5001",
+    "bill_node"         = "5002",
+    "james_node"        = "5003",
   }
 }
