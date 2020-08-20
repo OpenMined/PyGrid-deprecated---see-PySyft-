74e37e1f6109a6af5cb4b065fe0f4a52ae18202b
AmrMKayid <amrmkayid@gmail.com>
2020-08-21 01:40:01 +0200

    Add click Colors

--- apps/cli/utils.py
+++ apps/cli/utils.py
@@ -4,3 +4,25 @@ from types import SimpleNamespace
 class Config(SimpleNamespace):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+
+COLORS = SimpleNamespace(
+    **{
+        "black": "black",
+        "red": "red",
+        "green": "green",
+        "yellow": "yellow",
+        "blue": "blue",
+        "magenta": "magenta",
+        "cyan": "cyan",
+        "white": "white",
+        "bright_black": "bright_black",
+        "bright_red": "bright_red",
+        "bright_green": "bright_green",
+        "bright_yellow": "bright_yellow",
+        "bright_blue": "bright_blue",
+        "bright_magenta": "bright_magenta",
+        "bright_cyan": "bright_cyan",
+        "bright_white": "bright_white",
+    }
+)
