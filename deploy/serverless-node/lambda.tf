252b75383fa8a5d6fc4e489244c41da7895b50a4
sachin-101 <sachinkumar04428@gmail.com>
2020-08-24 10:29:17 +0530

    Separate resources into files

---
+++ deploy/serverless-node/lambda.tf
@@ -0,0 +1,44 @@
+locals {
+  function_path    = "../../apps/node/src/"
+  function_handler = "wsgi.app"
+}
+
+module "lambda" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "pygrid-node"
+  description   = "Node hosted by UCSF"
+  publish       = true    # To automate increasing versions
+
+  runtime     = "python3.7"
+  source_path = local.function_path
+  handler     = local.function_handler
+
+//  layers = [
+//    module.lambda_layer_all_dependencies.this_lambda_layer_arn,
+//    "arn:aws:lambda:us-east-1:934676248949:layer:pytorchv1-py36:1",  # pytorch 1.1.0, py3.6
+//  ]
+
+  #   tags = {
+  #     Name = ""
+  #   }
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service    = "apigateway"
+      source_arn = "${module.api_gateway.this_apigatewayv2_api_execution_arn}/*/*"
+    }
+  }
+}
+
+module "lambda_alias" {
+  source = "terraform-aws-modules/lambda/aws//modules/alias"
+
+  name = "prod"
+
+  function_name = module.lambda.this_lambda_function_name
+
+  # Set function_version when creating alias to be able to deploy using it,
+  # because AWS CodeDeploy doesn't understand $LATEST as CurrentVersion.
+  function_version = module.lambda.this_lambda_function_version
+}
--- deploy/serverless-node/main.tf
+++ deploy/serverless-node/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                  = "ap-south-1"
+  region                  = "us-east-1"
   shared_credentials_file = "$HOME/.aws/credentials"
 }
 
@@ -17,83 +17,5 @@ module "api_gateway" {
     "$default" = {
       lambda_arn = module.lambda.this_lambda_function_arn
     }
-  }
-}
-
-
-module "lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "~> 1.0"
-
-  function_name = "pygrid-node"
-  description   = "Node hosted by UCSF"
-  handler       = "wsgi.app"
-  runtime       = "python3.6"
-  publish       = true # To automate increasing versions
-
-  source_path = "../../apps/node/src/"
-
-#   tags = {
-#     Name = ""
-#   }
-
-  allowed_triggers = {
-    AllowExecutionFromAPIGateway = {
-      service    = "apigateway"
-      source_arn = "${module.api_gateway.this_apigatewayv2_api_execution_arn}/*/*"
-    }
-  }
-}
-
-module "lambda_alias" {
-  source  = "terraform-aws-modules/lambda/aws//modules/alias"
-  version = "~> 1.0"
-
-  name = "prod"
-
-  function_name = module.lambda.this_lambda_function_name
-
-  # Set function_version when creating alias to be able to deploy using it,
-  # because AWS CodeDeploy doesn't understand $LATEST as CurrentVersion.
-  function_version = module.lambda.this_lambda_function_version
-}
-
-# Data sources to get VPC and subnets
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-module "aurora" {
-  source = "terraform-aws-modules/rds-aurora/aws"
-
-  name                  = "aurora-serverless-database"
-  engine                = "aurora"
-  engine_mode           = "serverless"
-  replica_scale_enabled = false
-  replica_count         = 0
-
-  subnets       = data.aws_subnet_ids.all.ids
-  vpc_id        = data.aws_vpc.default.id
-  instance_type = "db.r4.large"
-
-  enable_http_endpoint = true   # Enable Data API
-
-  apply_immediately               = true
-  skip_final_snapshot             = true
-  storage_encrypted               = true
-
-  db_parameter_group_name         = var.db_parameter_group_name
-  db_cluster_parameter_group_name = var.db_cluster_parameter_group_name
-
-  scaling_configuration = {
-    auto_pause               = true
-    max_capacity             = 64  #ACU
-    min_capacity             = 2   #ACU
-    seconds_until_auto_pause = 300
-    timeout_action           = "ForceApplyCapacityChange"
   }
 }
