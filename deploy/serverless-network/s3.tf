49ee4dcdc34aa619b7afadcf33cd789d2e520f46
sachin-101 <sachinkumar04428@gmail.com>
2020-08-31 18:24:58 +0530

    Fix issues with s3 bucket creation

--- deploy/serverless-network/s3.tf
+++ deploy/serverless-network/s3.tf
@@ -1,3 +1,7 @@
+locals {
+  zip_path    = "lambda-layer/all-dep.zip"
+}
+
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "bucket-with-pygrid-network-dependencies"
@@ -10,6 +14,6 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_object" "lambda_dependencies" {
   bucket = aws_s3_bucket.bucket.bucket
-  key    = "${filemd5("lambda-layers/all-dep/all-dep.zip")}.zip"
-  source = "lambda-layers/all-dep/all-dep.zip"
+  key    = "${filemd5(local.zip_path)}.zip"
+  source = local.zip_path
 }
