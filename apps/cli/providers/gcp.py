a0403b9af258d6568d4ef6b0550cddde06ecdec5
AmrMKayid <amrmkayid@gmail.com>
2020-08-31 14:10:18 +0200

    Create gcp.py for managing deployment on GCP

---
+++ apps/cli/providers/gcp.py
@@ -0,0 +1,14 @@
+import click
+
+from ..utils import Config
+
+
+def get_gcp_config():
+    """Getting the configration required for deployment on GCP.
+
+    Returns:
+        Config: Simple Config with the user inputs
+    """
+    ## TODO:
+
+    return Config()
