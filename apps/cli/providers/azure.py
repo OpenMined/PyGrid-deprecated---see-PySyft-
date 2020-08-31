f0a0e59b2dc4eebf31e4ee844c66a6afad45502e
AmrMKayid <amrmkayid@gmail.com>
2020-08-31 14:10:28 +0200

    Create azure.py for managing deployment on AZURE

---
+++ apps/cli/providers/azure.py
@@ -0,0 +1,14 @@
+import click
+
+from ..utils import Config
+
+
+def get_azure_config():
+    """Getting the configration required for deployment on AZURE.
+
+    Returns:
+        Config: Simple Config with the user inputs
+    """
+    ## TODO:
+
+    return Config()
