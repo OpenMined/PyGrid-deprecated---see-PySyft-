c44176ef17daf395bbb341d462a20cdeff66ec67
AmrMKayid <amrmkayid@gmail.com>
2020-09-13 03:07:25 +0200

    Azure Provider class template

--- apps/cli/providers/azure.py
+++ apps/cli/providers/azure.py
@@ -1,14 +1,36 @@
 import click
 
 from ..utils import Config
+from .provider import *
 
 
-def get_azure_config():
-    """Getting the configration required for deployment on AZURE.
+class AZURE(Provider):
+    """Azure Cloud Provider."""
 
-    Returns:
-        Config: Simple Config with the user inputs
-    """
-    ## TODO:
+    def __init__(self, config):
+        super().__init__(config)
 
-    return Config()
+        self.config.azure = self.get_azure_config()
+
+    def build(self) -> bool:
+        pass
+
+    def deploy_network(
+        self, apply: bool = True,
+    ):
+        pass
+
+    def deploy_node(
+        self, apply: bool = True,
+    ):
+        pass
+
+    def get_azure_config(self) -> Config:
+        """Getting the configration required for deployment on AZURE.
+
+        Returns:
+            Config: Simple Config with the user inputs
+        """
+        ## TODO:
+
+        return Config()
