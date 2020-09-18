d17d02b859939310f77a06789c96306773e9e740
sachin-101 <sachinkumar04428@gmail.com>
2020-09-19 00:12:27 +0530

    Added IAM policies to access database from lambda

--- deploy/serverless-node/iam.tf
+++ deploy/serverless-node/iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "pygrid-network-lambda-role" {
-  name               = "pygrid-network-lambda-role"
+resource "aws_iam_role" "pygrid-node-lambda-role" {
+  name               = "pygrid-node-lambda-role"
   assume_role_policy = <<-EOF
   {
     "Version": "2012-10-17",
@@ -16,9 +16,11 @@ resource "aws_iam_role" "pygrid-network-lambda-role" {
   }
   EOF
 }
+# TODO: Remove all unncesssary actions from all policies
+
 
 resource "aws_iam_role_policy" "AmazonElasticFileSystemClientFullAccess" {
-  role   = aws_iam_role.pygrid-network-lambda-role.id
+  role   = aws_iam_role.pygrid-node-lambda-role.id
   policy = <<-EOF
   {
     "Version": "2012-10-17",
@@ -40,7 +42,7 @@ resource "aws_iam_role_policy" "AmazonElasticFileSystemClientFullAccess" {
 
 
 resource "aws_iam_role_policy" "AWSLambdaVPCAccessExecutionRole" {
-  role   = aws_iam_role.pygrid-network-lambda-role.id
+  role   = aws_iam_role.pygrid-node-lambda-role.id
   policy = <<-EOF
   {
     "Version": "2012-10-17",
@@ -56,6 +58,142 @@ resource "aws_iam_role_policy" "AWSLambdaVPCAccessExecutionRole" {
                 "ec2:DeleteNetworkInterface"
             ],
             "Resource": "*"
+        }
+    ]
+  }
+  EOF
+}
+
+
+
+resource "aws_iam_role_policy" "AmazonRDSDataFullAcess" {
+  name = "amazon-rds-data-full-acess"
+  role = aws_iam_role.pygrid-node-lambda-role.id
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "SecretsManagerDbCredentialsAccess",
+            "Effect": "Allow",
+            "Action": [
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:PutResourcePolicy",
+                "secretsmanager:PutSecretValue",
+                "secretsmanager:DeleteSecret",
+                "secretsmanager:DescribeSecret",
+                "secretsmanager:TagResource"
+            ],
+            "Resource": "arn:aws:secretsmanager:*:*:secret:rds-db-credentials/*"
+        },
+        {
+            "Sid": "RDSDataServiceAccess",
+            "Effect": "Allow",
+            "Action": [
+                "dbqms:CreateFavoriteQuery",
+                "dbqms:DescribeFavoriteQueries",
+                "dbqms:UpdateFavoriteQuery",
+                "dbqms:DeleteFavoriteQueries",
+                "dbqms:GetQueryString",
+                "dbqms:CreateQueryHistory",
+                "dbqms:DescribeQueryHistory",
+                "dbqms:UpdateQueryHistory",
+                "dbqms:DeleteQueryHistory",
+                "rds-data:ExecuteSql",
+                "rds-data:ExecuteStatement",
+                "rds-data:BatchExecuteStatement",
+                "rds-data:BeginTransaction",
+                "rds-data:CommitTransaction",
+                "rds-data:RollbackTransaction",
+                "secretsmanager:CreateSecret",
+                "secretsmanager:ListSecrets",
+                "secretsmanager:GetRandomPassword",
+                "tag:GetResources"
+            ],
+            "Resource": "*"
+        }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy" "CloudWatchLogsFullAccess" {
+  name = "cloud-watch-logs-full-access"
+  role = aws_iam_role.pygrid-node-lambda-role.id
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "logs:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy" "SecretManagerReadWrite" {
+  name = "secret-manager-read-write-access"
+  role = aws_iam_role.pygrid-node-lambda-role.id
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "secretsmanager:*",
+                "cloudformation:CreateChangeSet",
+                "cloudformation:DescribeChangeSet",
+                "cloudformation:DescribeStackResource",
+                "cloudformation:DescribeStacks",
+                "cloudformation:ExecuteChangeSet",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVpcs",
+                "kms:DescribeKey",
+                "kms:ListAliases",
+                "kms:ListKeys",
+                "lambda:ListFunctions",
+                "rds:DescribeDBClusters",
+                "rds:DescribeDBInstances",
+                "redshift:DescribeClusters",
+                "tag:GetResources"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Action": [
+                "lambda:AddPermission",
+                "lambda:CreateFunction",
+                "lambda:GetFunction",
+                "lambda:InvokeFunction",
+                "lambda:UpdateFunctionConfiguration"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:lambda:*:*:function:SecretsManager*"
+        },
+        {
+            "Action": [
+                "serverlessrepo:CreateCloudFormationChangeSet",
+                "serverlessrepo:GetApplication"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:serverlessrepo:*:*:applications/SecretsManager*"
+        },
+        {
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::awsserverlessrepo-changesets*",
+                "arn:aws:s3:::secrets-manager-rotation-apps-*/*"
+            ]
         }
     ]
   }
