c8ee5dafd61ab2a926d66c1bbe50afb0f75ee5d0
AmrMKayid <amrmkayid@gmail.com>
2020-09-22 13:29:47 +0200

    Update deploy base script

--- apps/cli/deploy.py
+++ apps/cli/deploy.py
@@ -1,4 +1,4 @@
-base_setup = f"""
+base_setup = f'''
 #!/bin/bash
 
 ## For debugging
@@ -6,17 +6,17 @@ base_setup = f"""
 exec &> log.out
 
 
-echo 'Simple Web Server for testing the deployment'
+echo "Simple Web Server for testing the deployment"
 sudo apt update -y
 sudo apt install apache2 -y
 sudo systemctl start apache2
-echo '''
+echo """
 <h1 style='color:#f09764; text-align:center'>
     OpenMined First Server Deployed via Terraform
 </h1>
-''' | sudo tee /var/www/html/index.html
+""" | sudo tee /var/www/html/index.html
 
-echo 'Setup Miniconda environment'
+echo "Setup Miniconda environment"
 
 sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 sudo bash miniconda.sh -b -p miniconda
@@ -27,15 +27,15 @@ source ~/.bashrc
 conda create -y -n pygrid python=3.7
 conda activate pygrid
 
-echo 'Install poetry...'
+echo "Install poetry..."
 pip install poetry
 
-echo 'Install GCC'
+echo "Install GCC"
 sudo apt-get install python3-dev -y
 sudo apt-get install libevent-dev -y
 sudo apt-get install gcc -y
 
-echo 'Cloning PyGrid'
+echo "Cloning PyGrid"
 git clone https://github.com/OpenMined/PyGrid
 
-"""
+'''
