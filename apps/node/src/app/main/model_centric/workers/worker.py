2c90cd76181d17f8985fc51b1fbe6cc4f0516f87
sachin-101 <sachinkumar04428@gmail.com>
2020-09-26 13:53:36 +0530

    Fix: Increase worker id to 1024 bytes

--- apps/node/src/app/main/model_centric/workers/worker.py
+++ apps/node/src/app/main/model_centric/workers/worker.py
@@ -14,7 +14,7 @@ class Worker(BaseModel):
 
     __tablename__ = "model_centric_worker"
 
-    id = db.Column(db.String(128), primary_key=True)
+    id = db.Column(db.String(1024), primary_key=True)
     ping = db.Column(db.Float)
     avg_download = db.Column(db.Float)
     avg_upload = db.Column(db.Float)
