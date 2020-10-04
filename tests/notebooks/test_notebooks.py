386463c624a0d071ea7c05681398e40763e81550
jmaunon <juanmixp@gmail.com>
2020-10-04 17:29:20 +0200

    Fix tutorials for github-action

--- tests/notebooks/test_notebooks.py
+++ tests/notebooks/test_notebooks.py
@@ -17,7 +17,7 @@ data_centric_intro_path = examples_path.joinpath(
 model_centric_mnist_path = examples_path.joinpath("examples", "model-centric")
 
 
-def test_notebooks_mnist_01():
+def test_notebooks_data_centric_mnist_01():
     """Test if notebook r"""
     notebook_mnist_01 = data_centric_mnist_path.joinpath(
         "01-FL-mnist-populate-a-grid-node.ipynb"
@@ -34,7 +34,7 @@ def test_notebooks_mnist_01():
     assert isinstance(res, nbformat.notebooknode.NotebookNode)
 
 
-def test_notebooks_mnist_02():
+def test_notebooks_data_centric_mnist_02():
     notebook_mnist_02 = data_centric_mnist_path.joinpath(
         "02-FL-mnist-train-model.ipynb"
     )
@@ -68,12 +68,28 @@ def test_notebooks_data_centric_introduction():
 
 
 def test_notebooks_model_centric_mnist_01():
-    notebook_intro_00 = model_centric_mnist_path.joinpath("01-Create-plan.ipynb")
+    nb_model_centric_mnist_01 = model_centric_mnist_path.joinpath(
+        "01-Create-plan.ipynb"
+    )
 
     res = pm.execute_notebook(
-        str(notebook_intro_00),
-        str(model_centric_mnist_path.joinpath("01-Create-plan_kk.ipynb")),
+        str(nb_model_centric_mnist_01),
+        os.devnull,
         dict(gridAddress=("localhost:" + worker_ports["Alice"])),
+    )
+
+    assert isinstance(res, nbformat.notebooknode.NotebookNode)
+
+
+def test_notebooks_model_centric_mnist_02():
+    nb_model_centric_mnist_02 = model_centric_mnist_path.joinpath(
+        "02-ExecutePlan.ipynb"
+    )
+
+    res = pm.execute_notebook(
+        str(nb_model_centric_mnist_02),
+        os.devnull,
+        dict(gridAddress=("ws://localhost:" + worker_ports["Alice"])),
     )
 
     assert isinstance(res, nbformat.notebooknode.NotebookNode)
