9c2506a5d45edd45552de66f1a209517593ec018
sachin-101 <sachinkumar04428@gmail.com>
2020-10-20 11:40:15 +0530

    Add database config as input parameters to cli
    * refactored input config functions
    * Separated them based on resource for which input is taken

--- apps/serverless_cli/providers/aws.py
+++ apps/serverless_cli/providers/aws.py
@@ -5,26 +5,9 @@ from PyInquirer import prompt
 from ..utils import Config, styles
 
 
-def vpc_regions_list():
-    vpc_client = boto3.client("ec2")
-    return [region["RegionName"] for region in vpc_client.describe_regions()["Regions"]]
-
-
-def az_list(region):
-    vpc_client = boto3.client("ec2", region_name=region)
-    return [
-        {"name": zone["ZoneName"]}
-        for zone in vpc_client.describe_availability_zones(
-            Filters=[{"Name": "region-name", "Values": [region]}]
-        )["AvailabilityZones"]
-    ]
-
-
-def get_aws_config() -> Config:
-    """Getting the configration required for deployment on AWS.
-
-    Returns:
-        Config: Simple Config with the user inputs
+def get_vpc_config() -> Config:
+    """Assists the user in entering configuration related to VPC.
+    :return:
     """
 
     region = prompt(
@@ -34,7 +17,10 @@ def get_aws_config() -> Config:
                 "name": "region",
                 "message": "Please select your desired AWS region",
                 "default": "us-east-1",
-                "choices": vpc_regions_list(),
+                "choices": [
+                    region["RegionName"]
+                    for region in boto3.client("ec2").describe_regions()["Regions"]
+                ],
             }
         ],
         style=styles.second,
@@ -46,13 +32,29 @@ def get_aws_config() -> Config:
                 "type": "checkbox",
                 "name": "av_zones",
                 "message": "Please select atleast two availability zones. (Not sure? Select the first two)",
-                "choices": az_list(region),
+                "choices": [
+                    {"name": zone["ZoneName"]}
+                    for zone in boto3.client(
+                        "ec2", region_name=region
+                    ).describe_availability_zones(
+                        Filters=[{"Name": "region-name", "Values": [region]}]
+                    )[
+                        "AvailabilityZones"
+                    ]
+                ],
             }
         ],
         style=styles.second,
     )["av_zones"]
 
-    ## VPC
+    return Config(region=region, av_zones=av_zones)
+
+
+def get_vpc_ip_config() -> Config:
+    """Assists the user in entering configuration related to IP address of VPC.
+    :return:
+    """
+
     cidr_blocks = prompt(
         [
             {
@@ -74,8 +76,59 @@ def get_aws_config() -> Config:
     )
 
     return Config(
-        region=region,
-        av_zones=av_zones,
         vpc_cidr_block=cidr_blocks["vpc_cidr_block"],
         subnet_cidr_block=cidr_blocks["subnet_cidr_block"],
     )
+
+
+def get_db_config() -> Config:
+    """Assists the user in entering configuration related the database.
+
+    :return:
+    """
+    username = prompt(
+        [
+            {
+                "type": "input",
+                "name": "username",
+                "message": "Please set a username for your Database",
+            }
+        ],
+        style=styles.second,
+    )["username"]
+
+    def get_password(msg, validate):
+        return prompt(
+            [
+                {
+                    "type": "password",
+                    "name": "password",
+                    "message": msg,
+                    "validate": validate,
+                }
+            ],
+            style=styles.second,
+        )["password"]
+
+    password = get_password(
+        msg="Enter a password for your Database (length > 8)",
+        validate=lambda x: True
+        if len(x) > 8
+        else "Password length should be greater than 8.",
+    )
+    re_password = get_password(
+        msg="Enter the password again",
+        validate=lambda x: True
+        if x == password
+        else "The passwords do not match. Please enter again",
+    )
+
+    return Config(username=username, password=password)
+
+
+def get_serverless_config() -> Config:
+    """Assists the user in entering configuration related to the resources in serverless deployment.
+
+    :return:
+    """
+    pass
