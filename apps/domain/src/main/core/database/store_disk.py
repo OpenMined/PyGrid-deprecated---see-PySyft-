d11832948ca6f7f0093d2440fd99fdb0f20bef85
Benardi Nunes <benardinunes@gmail.com>
2021-02-22 00:43:13 -0300

    Update DiskObjectStore to use syft Dataset

--- apps/domain/src/main/core/database/store_disk.py
+++ apps/domain/src/main/core/database/store_disk.py
@@ -5,7 +5,7 @@ from torch import Tensor
 from loguru import logger
 from flask import current_app as app
 from syft.core.common.uid import UID
-from syft.core.store import ObjectStore
+from syft.core.store import ObjectStore, Dataset
 from syft.core.common.serde import _deserialize
 from syft.core.store.storeable_object import StorableObject
 
@@ -16,11 +16,18 @@ from . import db, BaseModel
 ENCODING = "UTF-8"
 
 
-# from main.core.database.bin_storage.metadata import *
 def create_storable(
     _id: UID, data: Tensor, description: str, tags: Iterable[str]
 ) -> StorableObject:
     obj = StorableObject(id=_id, data=data, description=description, tags=tags)
+
+    return obj
+
+
+def create_dataset(
+    _id: UID, data: Iterable[StorableObject], description: str, tags: Iterable[str]
+) -> StorableObject:
+    obj = Dataset(id=_id, data=data, description=description, tags=tags)
 
     return obj
 
@@ -34,7 +41,7 @@ class DiskObjectStore(ObjectStore):
         db_path = uri[10:]
         return getsize(db_path)
 
-    def store(self, obj: StorableObject) -> None:
+    def store(self, obj: Dataset) -> None:
         bin_obj = BinaryObject(id=obj.id.value.hex, binary=obj.to_bytes())
         metadata = get_metadata(self.db)
         metadata.length += 1
@@ -47,12 +54,12 @@ class DiskObjectStore(ObjectStore):
         _id = item.value.hex
         return self.db.session.query(BinaryObject).get(_id) is not None
 
-    def __setitem__(self, key: UID, value: StorableObject) -> None:
+    def __setitem__(self, key: UID, value: Dataset) -> None:
         obj = self.db.session.query(BinaryObject).get(key.value.hex)
         obj.binary = value.to_bytes()
         self.db.session.commit()
 
-    def __getitem__(self, key: UID) -> StorableObject:
+    def __getitem__(self, key: UID) -> Dataset:
         try:
             obj = self.db.session.query(BinaryObject).get(key.value.hex)
             obj = _deserialize(blob=obj.binary, from_bytes=True)
@@ -61,7 +68,7 @@ class DiskObjectStore(ObjectStore):
             logger.trace(f"{type(self)} get item error {key} {e}")
             raise e
 
-    def get_object(self, key: UID) -> Optional[StorableObject]:
+    def get_object(self, key: UID) -> Optional[Dataset]:
         obj = None
         if self.db.session.query(BinaryObject).get(key.value.hex) is not None:
             obj = self.__getitem__(key)
@@ -90,14 +97,10 @@ class DiskObjectStore(ObjectStore):
         self.db.session.query(StorageMetadata).delete()
         self.db.session.commit()
 
-    def values(self) -> Iterable[StorableObject]:
-        # TODO _deserialize creates storable with no data or tags for StorableObject
+    def values(self) -> Iterable[Dataset]:
         binaries = self.db.session.query(BinaryObject.binary).all()
         binaries = [_deserialize(blob=b[0], from_bytes=True) for b in binaries]
         return binaries
-
-    def get_objects_of_type(self, obj_type: type) -> Iterable[StorableObject]:
-        return [v for v in self.values() if isinstance(v, obj_type)]
 
     def __str__(self) -> str:
         objs = self.db.session.query(BinaryObject).all()
