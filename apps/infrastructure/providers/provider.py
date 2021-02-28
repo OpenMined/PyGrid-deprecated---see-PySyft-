bd3f39413472b958a8fa3cc053a0b7ff6181899c
AmrMKayid <amrmkayid@gmail.com>
2021-03-01 00:07:28 +0200

    Update Provider

--- apps/infrastructure/providers/provider.py
+++ apps/infrastructure/providers/provider.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import time
 from pathlib import Path
+from types import SimpleNamespace
 
 import terrascript
 import terrascript.data as data
@@ -10,38 +11,45 @@ import terrascript.provider as provider
 import terrascript.resource as resource
 from terrascript import Module
 
-from apps.infrastructure.tf import Terraform
-from apps.infrastructure.utils import Config
-
-from types import SimpleNamespace
+from ..tf import Terraform
+from ..utils import Config
 
 
 class Provider:
-    def __init__(self, config):
-        self.app_dir = os.path.join(str(Path.home()), ".pygrid", "api", config.app.name)
+    def __init__(self, config: SimpleNamespace):
+        folder_name = f"{config.provider}-{config.app.name}-{config.app.id}"
+        self.app_dir = os.path.join(str(Path.home()), ".pygrid", "api", folder_name)
         os.makedirs(self.app_dir, exist_ok=True)
 
-        self.TF = Terraform()
+        self.TF = Terraform(dir=self.app_dir)
         self.tfscript = terrascript.Terrascript()
+        self.validated = False
+
+    def validate(self):
+        ## Writing the terraform file
+        self.TF.write(self.tfscript)
+        try:
+            self.TF.init()
+            self.TF.validate()
+            self.validated = True
+            return True
+        except subprocess.CalledProcessError as err:
+            return False
 
     def deploy(self):
-        # save the terraform configuration files
-        with open(f"{self.app_dir}/main.tf.json", "w") as tfjson:
-            json.dump(self.tfscript, tfjson, indent=2, sort_keys=False)
+        if not self.validated:
+            return (False, {})
 
         try:
-            self.TF.init(self.app_dir)
-            self.TF.validate(self.app_dir)
-            self.TF.apply(self.app_dir)
-            output = self.TF.output(self.app_dir)
+            self.TF.apply()
+            output = self.TF.output()
             return (True, output)
         except subprocess.CalledProcessError as err:
-            output = {"ERROR": err}
-            return (False, output)
+            return (False, {"ERROR": err})
 
     def destroy(self):
         try:
-            self.TF.destroy(self.app_dir)
+            self.TF.destroy()
             return True
         except subprocess.CalledProcessError as err:
             return False
