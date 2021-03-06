b1d595e77bfe17f560ca245752f83a93ca9cb1ed
sachin-101 <sachinkumar04428@gmail.com>
2021-03-06 19:00:29 +0530

    Moved worker-api to apps/src/domain

--- apps/domain/src/main/routes/data_centric/workers/routes.py
+++ apps/domain/src/main/routes/data_centric/workers/routes.py
@@ -495,3 +495,132 @@ def delete_node(current_user, worker_id):
         mimetype="application/json",
     )
 """
+
+import json
+import os
+import time
+from datetime import datetime
+from pathlib import Path
+
+from flask import Flask, Response, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+from .......infrastructure import AWS_Serverfull, Config, Provider
+from ....core.database import db
+from ....core.database.workers.worker import Worker, states
+from ..blueprint import dcfl_blueprint as dcfl_route
+
+
+def get_config(data):
+    """Reads environment variables from the domain instance to create a Config
+    object for deploying the worker."""
+    return Config(
+        app=Config(name="worker", count=1, id=db.session.query(Worker).count() + 1),
+        apps=[Config(name="worker", count=1)],
+        serverless=False,
+        websockets=False,
+        provider=os.environ["CLOUD_PROVIDER"],
+        vpc=Config(
+            region=os.environ["REGION"], instance_type=Config(**data["instance_type"])
+        ),
+    )
+
+
+@dcfl_route.route("/check")
+def check():
+    response = {"message": "Can access worker api"}
+    return Response(json.dumps(response), status=200, content_type="application/json")
+
+
+@dcfl_route.route("/workers/deploy", methods=["POST"])
+def create():
+    """Creates a worker.
+    This endpoint can be accessed by a user to create a new worker."""
+
+    # data = json.loads(request.json)
+    config = get_config(request.json)
+    print(config)
+
+    deployment = None
+    deployed = False
+    output = {}
+
+    if config.provider == "aws":
+        deployment = AWS_Serverfull(config=config)
+    elif config.provider == "azure":
+        pass
+    elif config.provider == "gcp":
+        pass
+
+    if deployment.validate():
+        worker = Worker(
+            id=config.app.id,
+            provider=config.provider,
+            region=config.vpc.region,
+            instance=config.vpc.instance_type.InstanceType,
+            state=states["creating"],
+        )
+        db.session.add(worker)
+        db.session.commit()
+
+        deployed, output = deployment.deploy()  # Deploy
+        # time.sleep(5)
+        # deployed = False
+        # output = {}
+
+        worker = Worker.query.get(config.app.id)
+        if deployed:
+            worker.state = states["success"]
+            worker.deployed_at = datetime.now()
+        else:
+            worker.state = states["failed"]
+        db.session.commit()
+
+    response = {"deloyed": deployed, "output": output}
+    return Response(json.dumps(response), status=200, mimetype="application/json")
+
+
+@dcfl_route.route("/workers", methods=["GET"])
+def get_workers():
+    """Get all deployed workers.
+    Only Node operators can access this endpoint.
+    """
+    workers = Worker.query.order_by(Worker.created_at).all()
+    return Response(
+        json.dumps([worker.as_dict() for worker in workers], default=str),
+        status=200,
+        mimetype="applications/json",
+    )
+
+
+@dcfl_route.route("/workers/<int:id>", methods=["GET"])
+def get_worker(id):
+    """Get specific worker data.
+    Only the Node owner and the user who created this worker can access this endpoint.
+    """
+    worker = Worker.query.get(id)
+    return Response(
+        json.dumps(worker.as_dict(), default=str),
+        status=200,
+        mimetype="applications/json",
+    )
+
+
+@dcfl_route.route("/workers/<int:id>", methods=["DELETE"])
+def delete_worker(id):
+    """Shut down specific worker.
+    Only the Node owner and the user who created this worker can access this endpoint.
+    """
+    worker = Worker.query.get(id)
+    if worker.state == states["success"]:
+        config = Config(provider=worker.provider, app=Config(name="worker", id=id))
+        success = Provider(config).destroy()
+        if success:
+            worker.state = states["destroyed"]
+            worker.destroyed_at = datetime.now()
+            db.session.commit()
+        return Response(
+            json.dumps({"deleted": success}), status=200, mimetype="application/json"
+        )
+    else:
+        return Response(status=404, mimetype="application/json")
--- apps/infrastructure/__init__.py
+++ apps/infrastructure/__init__.py
@@ -1,6 +1,3 @@
-from .api import *
-from .cli import *
 from .providers import *
 from .tf import *
 from .utils import *
-from .worker_api import *
