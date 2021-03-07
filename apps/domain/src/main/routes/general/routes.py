3ea940d995b633c50cb7be0dbfac1adf131b2d87
Ionesio Junior <ionesiojr@gmail.com>
2021-03-07 12:01:23 -0300

    - Update main route - Add new syft serialize method - Refactor node -> get_node()

--- apps/domain/src/main/routes/general/routes.py
+++ apps/domain/src/main/routes/general/routes.py
@@ -1,11 +1,12 @@
 from .blueprint import root_blueprint as root_route
-from ...core.node import node
-from ...core.task_handler import executor
+from ...core.node import get_node
 
 # syft absolute
 from syft.core.common.message import SignedImmediateSyftMessageWithReply
 from syft.core.common.message import SignedImmediateSyftMessageWithoutReply
 from syft.core.common.serde.deserialize import _deserialize
+from syft import serialize
+from syft.core.common.serde.serialize import _serialize
 
 from flask import request, Response
 import json
@@ -17,8 +18,7 @@ executor_running = False
 @root_route.route("/metadata", methods=["GET"])
 def metadata_route():
     response_body = {
-        "metadata": node.get_metadata_for_client()
-        .serialize()
+        "metadata": serialize(get_node().get_metadata_for_client())
         .SerializeToString()
         .decode("ISO-8859-1"),
     }
@@ -30,12 +30,12 @@ def root_route():
     data = request.get_data()
     obj_msg = _deserialize(blob=data, from_bytes=True)
     if isinstance(obj_msg, SignedImmediateSyftMessageWithReply):
-        reply = node.recv_immediate_msg_with_reply(msg=obj_msg)
-        r = Response(response=reply.serialize(to_bytes=True), status=200)
+        reply = get_node().recv_immediate_msg_with_reply(msg=obj_msg)
+        r = Response(response=_serialize(obj=reply, to_bytes=True), status=200)
         r.headers["Content-Type"] = "application/octet-stream"
         return r
     elif isinstance(obj_msg, SignedImmediateSyftMessageWithoutReply):
-        node.recv_immediate_msg_without_reply(msg=obj_msg)
+        get_node().recv_immediate_msg_without_reply(msg=obj_msg)
     else:
-        node.recv_eventual_msg_without_reply(msg=obj_msg)
+        get_node().recv_eventual_msg_without_reply(msg=obj_msg)
     return ""
