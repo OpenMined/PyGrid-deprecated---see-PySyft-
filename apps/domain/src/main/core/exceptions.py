470eadb5be463189df80da87a29b7f98a6d2e095
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-03-07 18:45:42 -0300

    Add RequestService and update node references

--- apps/domain/src/main/core/exceptions.py
+++ apps/domain/src/main/core/exceptions.py
@@ -72,3 +72,9 @@ class RequestError(PyGridError):
     def __init__(self):
         message = "Request ID not found!"
         super().__init__(message)
+
+class InvalidParameterValueError(PyGridError):
+    def __init__(self, message=""):
+        if not message:
+            message = "Passed paramater value not valid!"
+        super().__init__(message)
