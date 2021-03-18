d33b7ba801704aada2b4f41fbbf19df2e59dae31
AmrMKayid <amrmkayid@gmail.com>
2021-03-18 02:21:32 +0200

    Wrote terrascript classes to support Azurem
    -- terrascript 0.9.0 does not fully support azurem provider yet :')

---
+++ apps/domain/src/main/core/infrastructure/providers/azure/azure_ts.py
@@ -0,0 +1,30 @@
+# terrascript/azure/r.py
+import terrascript
+
+
+class azurerm(terrascript.Provider):
+    pass
+
+
+class azurerm_resource_group(terrascript.Resource):
+    pass
+
+
+class azurerm_virtual_network(terrascript.Resource):
+    pass
+
+
+class azurerm_subnet(terrascript.Resource):
+    pass
+
+
+class azurerm_network_interface(terrascript.Resource):
+    pass
+
+
+class azurerm_network_security_group(terrascript.Resource):
+    pass
+
+
+class azurerm_virtual_machine(terrascript.Resource):
+    pass
