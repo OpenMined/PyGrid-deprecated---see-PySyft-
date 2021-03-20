732978a44be12afc5be0c756db7fc778ea56921b
Ionesio Junior <ionesiojr@gmail.com>
2021-03-20 17:18:31 -0300

    Add new role manager properties

--- apps/domain/src/main/core/manager/role_manager.py
+++ apps/domain/src/main/core/manager/role_manager.py
@@ -15,6 +15,22 @@ class RoleManager(DatabaseManager):
         self._schema = RoleManager.schema
         self.db = database
 
+    @property
+    def user_role(self):
+        return self.first(name="User")
+
+    @property
+    def owner_role(self):
+        return self.first(name="Owner")
+
+    @property
+    def compliance_officer_role(self):
+        return self.first(name="Compliance Officer")
+
+    @property
+    def admin_role(self):
+        return self.first(name="Administrator")
+
     def first(self, **kwargs) -> Union[None, List]:
         result = super().first(**kwargs)
         if not result:
