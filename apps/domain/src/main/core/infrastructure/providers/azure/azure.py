fdad236638063afb6ebde2a76876065624278c75
AmrMKayid <amrmkayid@gmail.com>
2021-03-25 14:33:46 +0200

    Update Azure Provider class

--- apps/domain/src/main/core/infrastructure/providers/azure/azure.py
+++ apps/domain/src/main/core/infrastructure/providers/azure/azure.py
@@ -14,11 +14,10 @@ class AZURE(Provider):
 
     def __init__(self, config: SimpleNamespace) -> None:
         super().__init__(config.root_dir, "azure")
-
         self.config = config
 
         ##TODO(amr): terrascript does not support azurem right now
-        self.tfscript += terrascript.terraform(backend=terrascript.backend("azurerm"))
+        # self.tfscript += terrascript.terraform(backend=terrascript.backend("azurerm"))
         self.tfscript += azurerm(
             features={},
             subscription_id=self.config.azure.subscription_id,
@@ -27,18 +26,26 @@ class AZURE(Provider):
             tenant_id=self.config.azure.tenant_id,
         )
 
-        self.build()
-        self.build_instances()
-        print(self.tfscript)
+        self.build_resource_group()
+        self.build_virtual_network()
+        self.build_subnets()
+        self.build_public_ip()
 
-    def build(self) -> bool:
+        self.build_security_groups()
+        self.build_network_interface()
+
+        self.build_instances()
+        self.build_load_balancer()
+
+    def build_resource_group(self):
         self.resource_group = azurerm_resource_group(
-            "pygrid_resource_group",
-            name="pygrid_resource_group",
+            f"pygrid_resource_group",
+            name=f"pygrid_resource_group",
             location=self.config.azure.location,
         )
         self.tfscript += self.resource_group
 
+    def build_virtual_network(self):
         self.virtual_network = azurerm_virtual_network(
             f"pygrid_virtual_network",
             name=f"pygrid_virtual_network",
@@ -52,6 +59,7 @@ class AZURE(Provider):
         )
         self.tfscript += self.virtual_network
 
+    def build_subnets(self):
         self.azurerm_subnet = azurerm_subnet(
             f"pygrid_subnet",
             name=f"pygrid_subnet",
@@ -61,6 +69,103 @@ class AZURE(Provider):
         )
         self.tfscript += self.azurerm_subnet
 
+        self.availability_set = azurerm_availability_set(
+            f"pygrid_availability_set",
+            name=f"pygrid_availability_set",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            platform_fault_domain_count=self.config.app.count,
+            platform_update_domain_count=self.config.app.count,
+            managed=True,
+        )
+        self.tfscript += self.availability_set
+
+    def build_public_ip(self):
+        self.public_ip = azurerm_public_ip(
+            f"pygrid_public_ip",
+            name=f"pygrid_public_ip",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            allocation_method="Dynamic",
+            tags={
+                "name": "pygrid-public-ip",
+                "environment": "dev",
+            },
+        )
+        self.tfscript += self.public_ip
+
+    def build_security_groups(self):
+        self.network_security_group = azurerm_network_security_group(
+            f"pygrid_network_security_group",
+            name=f"pygrid_network_security_group",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            # security_rule=[
+            #     {
+            #         "name": "HTTPS",
+            #         "priority": 100,
+            #         "direction": "Inbound",
+            #         "access": "Allow",
+            #         "protocol": "Tcp",
+            #         "source_port_range": "443",
+            #         "destination_port_range": "443",
+            #         "source_address_prefix": "*",
+            #         "destination_address_prefix": "*",
+            #     },
+            #     {
+            #         "name": "HTTP",
+            #         "priority": 101,
+            #         "direction": "Inbound",
+            #         "access": "Allow",
+            #         "protocol": "Tcp",
+            #         "source_port_range": "80",
+            #         "destination_port_range": "80",
+            #         "source_address_prefix": "*",
+            #         "destination_address_prefix": "*",
+            #     },
+            #     {
+            #         "name": "PyGrid Domains",
+            #         "priority": 102,
+            #         "direction": "Inbound",
+            #         "access": "Allow",
+            #         "protocol": "Tcp",
+            #         "source_port_range": "5000",
+            #         "destination_port_range": "5999",
+            #         "source_address_prefix": "*",
+            #         "destination_address_prefix": "*",
+            #     },
+            #     {
+            #         "name": "PyGrid Workers",
+            #         "priority": 103,
+            #         "direction": "Inbound",
+            #         "access": "Allow",
+            #         "protocol": "Tcp",
+            #         "source_port_range": "6000",
+            #         "destination_port_range": "6999",
+            #         "source_address_prefix": "*",
+            #         "destination_address_prefix": "*",
+            #     },
+            #     {
+            #         "name": "PyGrid Networks",
+            #         "priority": 104,
+            #         "direction": "Inbound",
+            #         "access": "Allow",
+            #         "protocol": "Tcp",
+            #         "source_port_range": "7000",
+            #         "destination_port_range": "7999",
+            #         "source_address_prefix": "*",
+            #         "destination_address_prefix": "*",
+            #     },
+            # ],
+            tags={
+                "name": "pygrid-security-groups",
+                "environment": "dev",
+            },
+        )
+        self.tfscript += self.network_security_group
+
+    def build_network_interface(self):
+
         self.network_interface = azurerm_network_interface(
             f"pygrid_network_interface",
             name=f"pygrid_network_interface",
@@ -68,76 +173,22 @@ class AZURE(Provider):
             location=self.resource_group.location,
             ip_configuration={
                 "name": "internal",
-                "subnet_id": self.azurerm_subnet.id,
+                "subnet_id": var(self.azurerm_subnet.id),
                 "private_ip_address_allocation": "Dynamic",
+            },
+            tags={
+                "name": "pygrid-network-interface",
+                "environment": "dev",
             },
         )
         self.tfscript += self.network_interface
 
-        self.network_security_group = azurerm_network_security_group(
-            f"pygrid-network-security-group",
-            name=f"pygrid-network-security-group",
-            resource_group_name=self.resource_group.name,
-            location=self.resource_group.location,
-            security_rule=[
-                {
-                    "name": "HTTPS",
-                    "priority": 100,
-                    "direction": "Inbound",
-                    "access": "Allow",
-                    "protocol": "Tcp",
-                    "source_port_range": "443",
-                    "destination_port_range": "443",
-                    "source_address_prefix": "*",
-                    "destination_address_prefix": "*",
-                },
-                {
-                    "name": "HTTP",
-                    "priority": 100,
-                    "direction": "Inbound",
-                    "access": "Allow",
-                    "protocol": "Tcp",
-                    "source_port_range": "80",
-                    "destination_port_range": "80",
-                    "source_address_prefix": "*",
-                    "destination_address_prefix": "*",
-                },
-                {
-                    "name": "PyGrid Domains",
-                    "priority": 100,
-                    "direction": "Inbound",
-                    "access": "Allow",
-                    "protocol": "Tcp",
-                    "source_port_range": "5000",
-                    "destination_port_range": "5999",
-                    "source_address_prefix": "*",
-                    "destination_address_prefix": "*",
-                },
-                {
-                    "name": "PyGrid Workers",
-                    "priority": 100,
-                    "direction": "Inbound",
-                    "access": "Allow",
-                    "protocol": "Tcp",
-                    "source_port_range": "6000",
-                    "destination_port_range": "6999",
-                    "source_address_prefix": "*",
-                    "destination_address_prefix": "*",
-                },
-                {
-                    "name": "PyGrid Networks",
-                    "priority": 100,
-                    "direction": "Inbound",
-                    "access": "Allow",
-                    "protocol": "Tcp",
-                    "source_port_range": "7000",
-                    "destination_port_range": "7999",
-                    "source_address_prefix": "*",
-                    "destination_address_prefix": "*",
-                },
-            ],
+        self.ni_sec_association = azurerm_network_interface_security_group_association(
+            f"network_interface_security_group_association",
+            network_interface_id=var(self.network_interface.id),
+            network_security_group_id=var(self.network_security_group.id),
         )
-        self.tfscript += self.network_security_group
+        self.tfscript += self.ni_sec_association
 
     def build_instances(self):
         name = self.config.app.name
@@ -151,32 +202,92 @@ class AZURE(Provider):
                 name=name,
                 resource_group_name=self.resource_group.name,
                 location=self.resource_group.location,
-                network_interface_ids=[self.network_interface.id],
+                network_interface_ids=[var(self.network_interface.id)],
+                availability_set_id=var(self.availability_set.id),
                 vm_size="Standard_DS1_v2",  # TODO: get this config from user
                 # TODO: get config from user
                 storage_image_reference={
                     "publisher": "Canonical",
                     "offer": "UbuntuServer",
-                    "sku": "16.04-LTS",
+                    "sku": "18.04-LTS",
                     "version": "latest",
                 },
                 storage_os_disk={
-                    "name": "myosdisk1",
+                    "name": "OSDisk",
                     "caching": "ReadWrite",
                     "create_option": "FromImage",
                     "managed_disk_type": "Standard_LRS",
                 },
                 os_profile={
                     "computer_name": "hostname",
-                    "admin_username": "testadmin",
-                    "admin_password": "Password1234!",
+                    "admin_username": "openmined",
+                    "admin_password": "pswd123!",
+                    "custom_data": self.write_exec_script(app, index=count),
                 },
                 os_profile_linux_config={"disable_password_authentication": False},
-                custom_data=self.write_exec_script(app, index=count),
+                delete_os_disk_on_termination=True,
+                delete_data_disks_on_termination=True,
             )
 
             self.tfscript += instance
             self.instances.append(instance)
+
+    def build_load_balancer(self):
+        self.load_balancer = azurerm_lb(
+            f"pygrid_load_balancer",
+            name=f"pygrid_load_balancer",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            frontend_ip_configuration={
+                "name": "lb_public_ip",
+                "public_ip_address_id": var(self.public_ip.id),
+            },
+        )
+        self.tfscript += self.load_balancer
+
+        self.lb_backend_address = azurerm_lb_backend_address_pool(
+            f"pygrid_lb_backend_address",
+            name=f"pygrid_lb_backend_address",
+            loadbalancer_id=var(self.load_balancer.id),
+        )
+        self.tfscript += self.lb_backend_address
+
+    def build_database(self):
+        "https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/sql_database"
+        self.sql_server = azurerm_sql_server(
+            f"pygrid_sql_server",
+            name=f"pygrid_sql_server",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            version="12.0",
+            administrator_login=self.config.credentials.db.username,
+            administrator_login_password=self.config.credentials.db.password,
+        )
+        self.tfscript += self.sql_server
+
+        self.storage_account = azurerm_storage_account(
+            f"pygrid_storage_account",
+            name=f"pygrid_storage_account",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            account_tier="Standard",
+            account_replication_type="LRS",
+        )
+        self.tfscript += self.storage_account
+
+        self.database = azurerm_sql_database(
+            f"pygrid-{self.config.app.name}-database",
+            name=f"pygrid-{self.config.app.name}-database",
+            resource_group_name=self.resource_group.name,
+            location=self.resource_group.location,
+            server_name=self.sql_server.name,
+            extended_auditing_policy={
+                "storage_endpoint": self.storage_account.primary_blob_endpoint,
+                "storage_account_access_key": self.storage_account.primary_access_key,
+                "storage_account_access_key_is_secondary": True,
+                "retention_in_days": 6,
+            },
+        )
 
     def write_exec_script(self, app, index=0):
         ##TODO(amr): remove `git checkout pygrid_0.3.0` after merge
@@ -225,7 +336,7 @@ class AZURE(Provider):
             ## TODO(amr): remove this after poetry updates
             pip install pymysql
 
-            nohup ./run.sh --port {app.port}  --host {app.host}
+            nohup ./run.sh --port {app.port} --start_local_db
         """
         )
         return exec_script
