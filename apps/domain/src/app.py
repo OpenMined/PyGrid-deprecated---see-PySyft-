81bb3241c112a79bb9f4957649341b83b57e2497
Madhava Jay <me@madhavajay.com>
2021-03-31 16:09:42 +1000

    Fix missing testing mode for domain tests
    - Without testing mode the database had the wrong IDs for some entries

--- apps/domain/src/app.py
+++ apps/domain/src/app.py
@@ -59,7 +59,9 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
-def create_app(args, secret_key=DEFAULT_SECRET_KEY, debug=False) -> Flask:
+def create_app(
+    args, secret_key=DEFAULT_SECRET_KEY, debug=False, testing=False
+) -> Flask:
     """This method creates a new Flask App instance and attach it with some
     HTTP/Websocket bluetprints.
 
@@ -75,7 +77,7 @@ def create_app(args, secret_key=DEFAULT_SECRET_KEY, debug=False) -> Flask:
     app.config.from_object("config")
 
     # Create Domain APP
-    app = create_domain_app(app=app, args=args)
+    app = create_domain_app(app=app, args=args, testing=testing)
 
     app.debug = debug
     app.config["SECRET_KEY"] = secret_key
