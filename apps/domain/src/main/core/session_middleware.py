d17aab5c4a624c001de0d5228057b6a6e8ef07fe
Victor Perez <victor.perez.9889@gmail.com>
2021-05-26 12:06:23 +0100

    Session Middleware

---
+++ apps/domain/src/main/core/session_middleware.py
@@ -0,0 +1,35 @@
+# stdlib
+from json import dumps
+
+# third party
+from flask import Response
+from flask import current_app as app
+import jwt
+from werkzeug.wrappers import Request
+
+# grid relative
+from ..core.codes import RESPONSE_MSG
+from ..core.database import User
+from .database import SetupConfig
+from .database import db
+from .exceptions import AppInSleepyMode
+
+
+class SessionMiddleware(object):
+    def __init__(self, app, app_wsgi):
+        self.app = app
+        self.wsgi = app_wsgi
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response_body = {}
+
+        with self.app.app_context():
+            current_user = None
+            token = request.headers.get("token", None)
+            if token:
+                data = jwt.decode(token, app.config["SECRET_KEY"], algorithms="HS256")
+                current_user = db.session.query(User).get(data["id"])
+
+        environ["current_user"] = current_user
+        return self.wsgi(environ, start_response)
